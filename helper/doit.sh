13d26d11b858ebcd9f0825ce8112d9908439c0b4
Heiko Rommel <rommel@suse.com>
2011-02-03 12:17:00 +0000

    - fixed a typo in the compare script for VENDOR and URL checking (compare_vendor_and_disturl.pl) - fixed arguments to compare scripts in doit.sh

--- helper/doit.sh
+++ helper/doit.sh
@@ -6,7 +6,7 @@ mode="$1"
 resultdir="$2"
 
 category1="check_all_updated.pl check_from_same_srcrpm.pl check_vendor_and_disturl.pl"
-category2="comare_vendor_and_disturl.pl compare_all_updated.sh compare_from_same_srcrpm.sh"
+category2="compare_vendor_and_disturl.pl compare_all_updated.sh compare_from_same_srcrpm.sh"
 
 mydir="${0%/*}"
 PATH="$PATH:$mydir"
@@ -36,8 +36,9 @@ case "$mode" in
        done
        for helper in $category2; do
           progname=${helper##*/}
+          checkprog=${progname/check_/compare_}
           echo "launching $progname"
-          $helper $resultdir/$progname.before.error $resultdir/$progname.after.error 2> $resultdir/$progname.errors > $resultdir/$progname.out
+          $helper $resultdir/$checkprog.before.error $resultdir/$checkprog.after.error 2> $resultdir/$progname.errors > $resultdir/$progname.out
        done
       ;; 
     *) echo "$usage" ;;
