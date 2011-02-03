5a19e78de29789cfd3ce3efb18e9cbeb8badf7e8
Heiko Rommel <rommel@suse.com>
2011-02-03 11:54:53 +0000

    added a demonstration launcher script for the helper programs (doit.sh)

---
+++ helper/doit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+mode="$1"
+resultdir="$2"
+
+usage="${0##*/} ( before | after ) <result dir>"
+category1="check_all_updated.pl check_from_same_srcrpm.pl check_vendor_and_disturl.pl"
+category2="comare_vendor_and_disturl.pl compare_all_updated.sh compare_from_same_srcrpm.sh"
+
+if [ -z "$mode" -o -z "$resultdir" ]; then
+    echo "$usage"
+    exit 1
+fi
+
+mkdir -p "$resultdir" || exit 1
+
+case "$mode" in
+    before)
+       for helper in $category1; do
+          progname=${helper##*/}
+          echo "launching $progname"
+          if [ "$progname" = "check_all_updated.pl" ]; then helper="$helper --installed"; fi
+          ./$helper 2> $resultdir/$progname.before.error > $resultdir/$progname.before.out
+       done
+       ;;
+    after)
+       for helper in $category1; do
+          progname=${helper##*/}
+          echo "launching $progname"
+          if [ "$progname" = "check_all_updated.pl" ]; then helper="$helper --installed"; fi
+          ./$helper 2> $resultdir/$progname.after.error > $resultdir/$progname.after.out
+       done
+       for helper in $category2; do
+          progname=${helper##*/}
+          echo "launching $progname"
+          ./$helper $resultdir/$progname.before.error $resultdir/$progname.after.error 2> $resultdir/$progname.errors > $resultdir/$progname.out
+       done
+      ;; 
+    *) echo "$usage" ;;
+esac
