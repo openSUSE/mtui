9d4a1e13454b9593f7422535103b7722d4dffaad
Heiko Rommel <rommel@suse.com>
2011-05-26 07:36:19 +0000

    added helper scripts for mtui to check for multi-owned files and symlinks

---
+++ helper/check_multiple-owners.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$1" = "-h" ]; then
+cat <<EOF
+
+$0 takes no arguments
+
+$0 checks if there are any files or symlinks in the file lists of the installed RPMs that are owned by more than package. Strictly speaking, both cases are errors. However, SUSE tends to reuse symlinks from multiple packages to implement flexibility (like /boot/initrd owned by all installed kernel flavours and /etc/alternatives/* owned by all alternatives). Therefore, only multi-owned files are considered as error. Multi-owned symlinks are printed JFYI.
+
+This check is necessary since rpm -V does not detect such packaging errors and they seldom show up in the update log.
+
+EOF
+exit 0
+fi
+
+rpm -qal | 
+grep -v "(contains no files)" | 
+sort -t / | 
+uniq -c | 
+while read count path; do 
+   if [ $count -gt 1 ]; then 
+      if [ ! -d $path ];  then 
+         owners=`rpm -qf --qf "%{NAME} " $path | sort -t -`
+         if [ ! -L $path ]; then
+            echo "ERROR: file $path owned by ($owners)" 1>&2
+         else
+            echo "INFO: symlink $path owned by ($owners)"
+         fi
+      fi 
+      
+   fi
+done
+
---
+++ helper/compare_multiple-owners.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+progname=${0##*/}
+
+if [ -z "$1" -o -z "$2" ]; then
+   echo "usage: $progname <logfile before the update> <logfile after the update>"
+   exit 2
+fi
+
+temp1=$(mktemp /tmp/$progname.XXXXXX)
+test -s "$1" && grep -E "^ERROR:" "$1"  > $temp1
+num1=$(wc -l $temp1 | cut -d ' ' -f 1)
+
+temp2=$(mktemp /tmp/$progname.XXXXXX)
+test -s "$2" && grep -E "^ERROR:" "$2"  > $temp2
+num2=$(wc -l $temp2 | cut -d ' ' -f 1)
+
+trap "rm -f $temp1 $temp2" SIGINT SIGKILL EXIT
+
+newmismatches=$(diff -Nur "$temp1" "$temp2" | grep -E "^\+")
+gonemismatches=$(diff -Nur "$temp1" "$temp2" | grep -E "^\-")
+
+if [ -n "$newmismatches" ]; then
+   (
+   echo "ERROR: found new mismatches after update (before:$num1 vs after:$num2):" 
+   diff -Nur "$1" "$2"
+   ) >&2 
+   exit 1
+fi
+
+if [ -n "$gonemismatches" ]; then
+   echo "INFO: good, some mismatches disappeared after update (before:$num1 vs after:$num2):"
+   diff -Nur "$1" "$2"
+   exit 0
+fi
+
