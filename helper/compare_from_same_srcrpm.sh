49012ed88be35a26552860bda4792175e7742586
Heiko Rommel <rommel@suse.com>
2013-01-03 16:36:11 +0000

    reverted a change in check_from_same_srcrpm.pl (filtering for packages of the update) bco there was risk of not catching some errors which could be detected by the original version; fixed the output so that the package lists for one src rev are sorted
    improved compare_from_same_srcrpm.sh to catch cases were errors
    were replaced by new - but different - errors of the src rpm
    
    adjusted the standalone checker script (doit.sh) to the recent renaming
    (*new_dependencies*)

--- helper/compare_from_same_srcrpm.sh
+++ helper/compare_from_same_srcrpm.sh
@@ -7,32 +7,25 @@ if ! [ -f "$1" -a -f "$2" ]; then
    exit 2
 fi
 
-temp1=$(mktemp /tmp/$progname.XXXXXX)
-test -s "$1" && grep -E "^ERROR" "$1"  > $temp1
-num1=$(wc -l $temp1 | cut -d ' ' -f 1)
-
-temp2=$(mktemp /tmp/$progname.XXXXXX)
-test -s "$2" && grep -E "^ERROR" "$2"  > $temp2
-num2=$(wc -l $temp2 | cut -d ' ' -f 1)
-
-trap "rm -f $temp1 $temp2" SIGINT SIGKILL EXIT
-
-newmismatches=$(diff -Nur "$temp1" "$temp2" | grep -E "^\+ERROR")
-gonemismatches=$(diff -Nur "$temp1" "$temp2" | grep -E "^\-ERROR")
+newmismatches=$(diff -Nur "$1" "$2" | grep -E "^\+(ERROR:|\s*[[:xdigit:]]{32,})")
+gonemismatches=$(diff -Nur "$1" "$2" | grep -E "^\-(ERROR:|\s*[[:xdigit:]]{32,})")
 
 if [ -n "$newmismatches" ]; then
    (
-   echo "ERROR: found new mismatches after update (before:$num1 vs after:$num2):" 
-   #diff -Nur "$1" "$2"
-   diff -Nur "$temp1" "$temp2" | grep -E "^\+ERROR"
+   echo "ERROR: found new (or different) errors after update:"
+   cat <<EOF
+$newmismatches
+EOF
+   #echo $newmismatches
    ) >&2 
    exit 1
 fi
 
 if [ -n "$gonemismatches" ]; then
-   echo "INFO: good, some mismatches disappeared after update (before:$num1 vs after:$num2):"
-   #diff -Nur "$1" "$2"
-   diff -Nur "$temp1" "$temp2" | grep -E "^\-ERROR"
+   echo "INFO: good, some errors disappeared after update:"
+   cat <<EOF
+$gonemismatches
+EOF
    exit 0
 fi
 
