2cccf45bec8010182f9b8f66ef8c0b7cea4f961b
Christian Kornacker <ckornacker@suse.com>
2013-01-24 15:55:18 +0000

    - add check script for automated ctcs2 testruns.   If a matching testsuite is found, run ctcs2 regression testing.

---
+++ helper/check_ctcs_testsuite.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$1" -o "$1" = "-h" ]; then
+cat <<EOF
+
+$0 <md5sum>
+
+$0 runs ctcs testsuites if a matching package is found
+
+EOF
+exit 2
+fi
+
+MD5=$1
+PATCHINFO_URL="http://hilbert.nue.suse.com/abuildstat/patchinfo/$MD5"
+
+blacklist="qa_test_lvm2"
+
+for subdir in $(wget -q "$PATCHINFO_URL" -O - | grep DIR | sed -e 's,.*href="\([^"]*\)/">.*,\1,g' | grep -v ^doc$ | grep -v patchinfo$); do 
+   for package in $(wget -q "$PATCHINFO_URL/$subdir" -O - | grep rpm | grep -v delta | sed -e 's,.*href="\([^"]*\)">.*,\1,g'); do
+      list="$list $package"
+   done
+done
+
+testsuites=$(zypper se -s qa_test -t package | grep qa_test | awk -F\| '{sub(/ qa_test_/,""); print $2 }' | sort -u)
+
+# remove blacklisted testsuites
+for testsuite in $blacklist; do
+    testsuites=${testsuites/$testsuite/}
+done
+
+for testsuite in $testsuites; do
+    if [[ $list =~ $testsuite ]]; then
+        zypper -n ref >/dev/null 2>&1
+        zypper -n in qa_test_$testsuite qa_lib_ctcs2 >/dev/null 2>&1
+        if [ -x /usr/share/qa/tools/test_$testsuite-run ]; then
+            export TESTS_LOGDIR=/var/log/qa/$MD5
+            /usr/share/qa/tools/test_$testsuite-run
+        fi
+    fi
+done
---
+++ helper/compare_ctcs_testsuite.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+progname=${0##*/}
+
+if [ -z "$1" -o -z "$2" ]; then
+   echo "usage: $progname <logfile before the update> <logfile after the update>"
+   exit 2
+fi
+
+# strip colors from output
+sed -i -e 's/\x1b\[[0-9]*m//g' "$1" "$2"
+
+before=$(grep -A 1 "^Tests passed:" $1 | tail -n 1)
+after=$(grep -A 1 "^Tests passed:" $2 | tail -n 1)
+
+if [ -z "$before" ]; then
+    # no succeeded testcases prior to the update found, finding
+    # regressions not possible. exit with NOT RUN
+    exit 3
+fi
+
+for testcase in $after; do
+    before=${before/$testcase/}
+done
+
+before=$(echo $before)
+
+if [ ! -z "$before" ]; then
+    echo "CTCS2 testcase regressions: $before"
+    exit 1
+else
+    exit 0
+fi
