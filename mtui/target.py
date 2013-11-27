openSUSE/mtui

commit 9a674dd24f47a8caf22f3de15e4bbfd164f1726e

cleanup: dead code
that OSError is catched inside the self.put call as EnvironmentError

mtui/target.py
@@ -133,8 +133,6 @@ class Target(object):
                     filename = os.path.join(datadir, 'helper', 'rep-clean', item)
                     destination = os.path.join(tempdir, item)
                     self.put(filename, destination)
-            except OSError:
-                out.error('missing rep-clean.sh script')
             else:
                 scriptfile = os.path.join(tempdir, 'rep-clean.sh')
                 conffile = os.path.join(tempdir, 'rep-clean.conf')

commit ddbdb8247a92068cfa79575f0b0429f8372a3f04

fix: syntax error introduced by 9a674dd

mtui/target.py
@@ -128,15 +128,14 @@ class Target(object):
             # copy over local rep-clean script if rep-clean isn't found
             datadir = config.datadir
             tempdir = config.target_tempdir
-            try:
-                for item in ['rep-clean.sh', 'rep-clean.conf']:
-                    filename = os.path.join(datadir, 'helper', 'rep-clean', item)
-                    destination = os.path.join(tempdir, item)
-                    self.put(filename, destination)
-            else:
-                scriptfile = os.path.join(tempdir, 'rep-clean.sh')
-                conffile = os.path.join(tempdir, 'rep-clean.conf')
-                command = '%s -F %s' % (scriptfile, conffile)
+            for item in ['rep-clean.sh', 'rep-clean.conf']:
+                filename = os.path.join(datadir, 'helper', 'rep-clean', item)
+                destination = os.path.join(tempdir, item)
+                self.put(filename, destination)
+
+            scriptfile = os.path.join(tempdir, 'rep-clean.sh')
+            conffile = os.path.join(tempdir, 'rep-clean.conf')
+            command = '%s -F %s' % (scriptfile, conffile)
         else:
             repclean.close()
 