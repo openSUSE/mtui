openSUSE/mtui

commit 9caba786433ad96c0efb14bb3eebbe54c1563e79

Refactor script_hook
Into saner interfaces with more smaller objects representing the scripts

mtui/prompt.py
@@ -19,6 +19,9 @@ import getpass
 import shutil
 from traceback import print_exc
 from os.path import join
+from os.path import isfile
+from os.path import basename
+from os.path import splitext
 
 from datetime import datetime
 from traceback import print_exc
@@ -39,6 +42,7 @@ from mtui.template import OBSUpdateID
 from mtui.template import SwampUpdateID
 from mtui import updater
 from mtui.utils import requires_update
+from mtui.utils import ass_is, ass_isL
 
 from distutils.version import StrictVersion
 
@@ -2288,7 +2292,7 @@ class CommandPrompt(cmd.Cmd):
             if missing and input('there were missing packages. cancel update process? (y/N) ', ['y', 'yes'], self.interactive):
                 return
 
-            script_hook(targets, 'pre', os.path.dirname(self.metadata.path), str(self.metadata.id))
+            self.metadata.script_hooks(PreScript).run(targets.values())
 
             out.info('updating')
 
@@ -2335,9 +2339,9 @@ class CommandPrompt(cmd.Cmd):
             if missing and input("some packages haven't been updated. cancel update process? (y/N) ", ['y', 'yes'], self.interactive):
                 return
 
-            script_hook(targets, 'post', self.metadata.report_wd(), str(self.metadata.id))
-            script_hook(targets, 'compare', self.metadata.report_wd(), str(self.metadata.id))
-            FileDelete(targets, self.metadata.target_wd('output')).run()
+            self.metadata.script_hooks(PostScript).run(targets.values())
+            self.metadata.script_hooks(CompareScript).run(targets.values())
+            FileDelete(targets.values(), self.metadata.target_wd('output')).run()
 
         Notification('MTUI', 'updating %s finished' % self.session).show()
         out.info('done')
@@ -2747,90 +2751,186 @@ class CommandPrompt(cmd.Cmd):
         out.error('failed to parse command: %s %s' % (method.__name__.replace('do_', ''), args))
         print '%s: %s' % (method.__name__.replace('do_', ''), method.__doc__)
 
-
-def script_hook(targets, which, templatedir, md5):
+class Script(object):
     """
-    :type md5: str
+    :type subdir: str
+    :param subdir: subdirectory in the L{TestReport.scripts_wd} where the
+        scripts are located.
+
+        Note: also used as a "type of the script" and can be shown to
+        the user.
+
+    FIXME: should be an abstract attribute
     """
-    # this hook seriously needs a rewrite
 
-    if which not in ['post', 'pre', 'compare']:
-        return
+    def __init__(self, tr, path, log, file_uploader, cmd_runner):
+        """
+        :type path: str
+        :param path: absolute path to the script
+        """
+        self.path = path
+        self.name = basename(path)
+        self.testreport = tr
+        self.log = log
+        self.file_uploader = file_uploader
+        self.cmd_runner = cmd_runner
 
-    output_dir = os.path.join(templatedir, 'output', 'scripts')
-    remote_dir = os.path.join(config.target_tempdir, md5)
+    def __repr__(self):
+        return "<{0}.{1} {2} for {3}>".format(
+            self.__module__,
+            self.__class__.__name__,
+            self.path,
+            repr(self.testreport)
+        )
 
-    scriptdir = os.path.join(templatedir, 'scripts', which)
+    def __str__(self):
+        return "{0} script {1}".format(
+            self.subdir,
+            self.name,
+        )
 
-    if not os.path.isdir(scriptdir):
-        out.warning('%s scripts not found in %s' % (which, scriptdir))
-        return
+    @classmethod
+    def absolute_subdir(cls, tr):
+        """
+        :type tr: L{TestReport}
+        """
+        return tr.scripts_wd(cls.subdir)
 
-    for script in os.listdir(scriptdir):
-        local_file = os.path.join(scriptdir, script)
-        remote_file = '%s.%s' % (which, script)
-
-        if not os.path.isfile(local_file):
-            continue
-
-        out.info('preparing script %s' % script)
+    def run(self, targets):
+        """
+        :type targets: [L{Target}]
+        """
+        ass_isL(targets, TargetI)
 
         try:
-            if which == 'compare':
-                for target in targets:
-                    suffix = script.rpartition('.')[2]
-                    prename = '%s/pre.%s.%s' % (output_dir, script.replace('compare_', 'check_'), target)
-                    postname = '%s/post.%s.%s' % (output_dir, script.replace('compare_', 'check_'), target)
-                    prename = glob.glob(prename.replace('.%s.' % suffix, '*'))[0]
-                    postname = glob.glob(postname.replace('.%s.' % suffix, '*'))[0]
-                    comparescript = os.path.join(templatedir, 'scripts', 'compare', script)
-                    command = [comparescript, prename, postname]
-
-                    out.debug('running %s' % str(command))
-                    stdout = stderr = None
-                    try:
-                        sub = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        (stdout, stderr) = sub.communicate()
-                        exitcode = sub.wait()
-                    except Exception as error:
-                        out.critical('running compare script failed: %s' % str(error))
-                        exitcode = 1
-
-                    if exitcode == 1:
-                        out.critical('testcase %s failed: %s\n%s' % (script, str(command), stdout))
-                        if stderr:
-                            print 'stderr:', stderr
-
-                    if exitcode == 2:
-                        out.warning('internal error in testcase %s: %s' % (script, str(command)))
-
-                    targets[target].log.append([' '.join(command), str(stdout), str(stderr), exitcode, 0])
-            else:
-
-                FileUpload(targets, local_file, os.path.join(remote_dir, remote_file)).run()
-                RunCommand(targets, '%s/%s %s' % (remote_dir, remote_file, md5)).run()
-
-                try:
-                    os.makedirs(output_dir)
-                except OSError as error:
-                    if error.errno == errno.EEXIST:
-                        pass
-                except Exception as error:
-                    out.critical('failed to create directories: %s' % str(error))
-                    return
-
-                for target in targets:
-                    filename = os.path.join(output_dir, '%s.%s' % (remote_file, target))
-                    try:
-                        f = open(filename, 'w')
-                        f.write(targets[target].lastout())
-                        f.write(targets[target].lasterr())
-                        f.close()
-                    except IOError as error:
-                        out.error('failed to write script output to %s: %s' % (filename, error.strerror))
+            self.log.info('running {0}'.format(self))
+            self._run(targets)
         except KeyboardInterrupt:
-            out.warning('skipping script %s' % script)
-            continue
+            self.log.warning('skipping {0}'.format(self))
+            return
+
+    def results_wd(self, *path, **kw):
+        return self.testreport.report_wd(
+            'output',
+            'scripts',
+            *path,
+            **kw
+        )
+
+    def _filename(self, target = None, subdir = None):
+        """
+        :returns: str "fully qualified" file name
+        """
+        ass_is(target, TargetI, True)
+
+        if not subdir:
+            subdir = self.subdir
+
+        xs = [subdir, splitext(self.name)[0]]
+        if target:
+            xs.append(target.hostname)
+
+        return ".".join(xs)
+
+class PreScript(Script):
+    subdir = "pre"
+
+    def remote_path(self):
+        return self.testreport.target_wd(self._filename())
+
+    def result_file(self, target):
+        """
+        :type target: L{TargetI} instance
+        """
+        ass_is(target, TargetI)
+        return self.results_wd(self._filename(target), filepath = True)
+
+    def _run(self, targets):
+        ass_isL(targets, TargetI)
+
+        self.file_uploader(
+            targets,
+            self.path,
+            self.remote_path(),
+        ).run()
+
+        self.cmd_runner(
+            dict([(t.hostname, t) for t in targets]),
+            "{0} {1}".format(self.remote_path(), self.testreport.id)
+        ).run()
+
+        for t in targets:
+            fname = self.result_file(t)
+            try:
+                with open(fname, 'w') as f:
+                    f.write(t.lastout())
+                    f.write(t.lasterr())
+            except IOError as e:
+                self.log.error(messages.FailedToWriteScriptResult(fname, e))
+
+class PostScript(PreScript):
+    subdir = "post"
+
+class CompareScript(Script):
+    subdir = "compare"
+
+    def _run(self, ts):
+        ass_isL(ts, TargetI)
+        for t in ts:
+            self._run_single_target(t)
+
+    def _run_single_target(self, t):
+        ass_is(t, TargetI)
+
+        pre = self.results_wd(self._filename(
+                subdir = PreScript.subdir,
+                target = t,
+            ).replace("compare_", "check_"),
+            filepath = True
+        )
+
+        post = self.results_wd(self._filename(
+                subdir = PostScript.subdir,
+                target = t,
+            ).replace("compare_", "check_"),
+            filepath = True
+        )
+
+        argv = [
+            self.path,
+            pre,
+            post,
+        ]
+
+        self.log.debug("running {0}".format(argv))
+        stdout = stderr = None
+        try:
+            p = subprocess.Popen(
+                argv,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE
+            )
+        except Exception as e:
+            t.log.append(' '.join(argv), '', '', 0x100, 0)
+            self.log.critical(messages.StartingCompareScriptError(e, argv))
+
+        (stdout, stderr) = p.communicate()
+        rc = p.wait()
+
+        t.log.append([' '.join(argv), str(stdout), str(stderr), rc, 0])
+
+        if rc == 0:
+            return
+
+        if rc == 2:
+            logger, msg = self.log.critical, messages.CompareScriptCrashed
+        else:
+            logger, msg = self.log.warning, messages.CompareScriptFailed
+
+        assert callable(logger), "{0!r} not callable".format(logger)
+
+        logger(msg(argv, stdout, stderr, rc))
+
 
 def enabled_targets(targets):
     temporary_targets = {}

mtui/messages.py
@@ -51,3 +51,50 @@ class MissingPackagesError(UserError):
 class TestReportNotLoadedError(UserError):
     def __str__(self):
         return 'TestReport not loaded'
+
+class FailedToWriteScriptResult(UserMessage):
+    def __init__(self, path, reason):
+        self.path = path
+        self.reason = reason
+
+    def __str__(self):
+        return "failed to write script output to {0}: {1}".format(
+            self.path,
+            self.reason,
+        )
+
+class StartingCompareScriptError(UserMessage):
+    def __init__(self, reason, argv):
+        self.reason = reason
+        self.argv = argv
+
+    def __str__(self):
+        return "Starting compare script {0!r} failed: {1}".format(
+            self.argv,
+            self.reason
+        )
+
+class CompareScriptError(UserMessage):
+    def __init__(self, argv, stderr, stdout, rc):
+        self.argv = argv
+        self.stderr = stderr
+        self.stdout = stdout
+        self.rc = rc
+
+    def __str__(self):
+        raise NotImplementedError
+
+class CompareScriptFailed(CompareScriptError):
+    def __str__(self):
+        return "Compare script {0!r} failed: rc = {1} err = {2!r}".format(
+            self.argv,
+            self.rc,
+            self.stderr,
+        )
+
+class CompareScriptCrashed(CompareScriptError):
+    def __str__(self):
+        return "Compare script {0!r} crashed: {1!r}".format(
+            self.argv,
+            self.stderr,
+        )

mtui/target.py
@@ -27,6 +27,7 @@ from mtui.config import *
 from mtui.rpmver import RPMVersion
 from mtui import messages
 from mtui.utils import unwords
+from mtui.utils import ass_is, ass_isL
 
 out = logging.getLogger('mtui')
 
@@ -343,7 +344,10 @@ class TargetLock(object):
 
         return True
 
-class Target(object):
+class TargetI(object):
+    pass
+
+class Target(TargetI):
     def __init__(self, hostname, system, packages=[], state='enabled',
         timeout=300, exclusive=False, connect=True, logger=None,
         lock=TargetLock, connection=Connection):
@@ -819,88 +823,79 @@ class ThreadedMethod(threading.Thread):
                 except ValueError:
                     pass  # already removed by ctrl+c
 
+class ThreadedTargetGroup(object):
+    def __init__(self, targets):
+        ass_isL(targets, TargetI)
 
-class FileDelete(object):
-
-    def __init__(self, targets, path):
         self.targets = targets
-        self.path = path
+
+    def mk_thread(self):
+        thread = ThreadedMethod(queue)
+        thread.setDaemon(True)
+        thread.start()
+
+    def mk_threads(self):
+        for _ in range(0, len(self.targets)):
+            self.mk_thread()
 
     def run(self):
-        for target in self.targets:
-            thread = ThreadedMethod(queue)
-            thread.setDaemon(True)
-            thread.start()
-
-        for target in self.targets:
-            try:
-                queue.put([self.targets[target].remove, [self.path]])
-            except KeyboardInterrupt:
-                pass
+        self.mk_threads()
+        self.setup_queue()
 
         while queue.unfinished_tasks:
             spinner()
 
         queue.join()
 
+    def setup_queue(self):
+        for t in self.targets:
+            queue.put(self.mk_cmd(t))
 
-class FileUpload(object):
+class FileDelete(ThreadedTargetGroup):
+    def __init__(self, targets, path):
+        super(FileDelete, self).__init__(targets)
+        self.path = path
 
+    def mk_cmd(self, t):
+        ass_is(t, TargetI)
+        return [t.remove, [self.path]]
+
+class FileUpload(ThreadedTargetGroup):
     def __init__(self, targets, local, remote):
-        self.targets = targets
+        super(FileUpload, self).__init__(targets)
         self.local = local
         self.remote = remote
 
-    def run(self):
-        for target in self.targets:
-            thread = ThreadedMethod(queue)
-            thread.setDaemon(True)
-            thread.start()
+    def mk_cmd(self, t):
+        ass_is(t, TargetI)
+        return [t.put, [self.local, self.remote]]
 
-        for target in self.targets:
-            try:
-                queue.put([self.targets[target].put, [self.local, self.remote]])
-            except KeyboardInterrupt:
-                pass
-
-        while queue.unfinished_tasks:
-            spinner()
-
-        queue.join()
-
-
-class FileDownload(object):
-
+class FileDownload(ThreadedTargetGroup):
     def __init__(self, targets, remote, local, postfix=False):
-        self.targets = targets
+        ass_isL(targets, TargetI)
+        super(FileDownload, self).__init__(targets)
+
         self.remote = remote
         self.local = local
         self.postfix = postfix
 
-    def run(self):
-        for target in self.targets:
-            thread = ThreadedMethod(queue)
-            thread.setDaemon(True)
-            thread.start()
+    def local_name(self, t):
+        """
+        :type t: L{TargetI} instance
+        """
+        if not self.postfix:
+            return self.local
 
-        for target in self.targets:
-            try:
-                if self.postfix:
-                    queue.put([self.targets[target].get, [self.remote, '%s.%s' % (self.local, target)]])
-                else:
-                    queue.put([self.targets[target].get, [self.remote, self.local]])
-            except KeyboardInterrupt:
-                pass
+        return '{0}.{1}'.format(self.local, t.hostname)
 
-        while queue.unfinished_tasks:
-            spinner()
-
-        queue.join()
-
+    def mk_cmd(self, t):
+        ass_is(t, TargetI)
+        return [t.get, [self.remote, self.local_name(t)]]
 
 class RunCommand(object):
-
     def __init__(self, targets, command):
+        ass_isL(targets.values(), TargetI)
+
         self.targets = targets
         self.command = command
 

mtui/template.py
@@ -14,6 +14,9 @@ from abc import abstractmethod
 from datetime import date
 
 from mtui.target import Target
+from mtui.target import TargetI
+from mtui.target import RunCommand
+from mtui.target import FileUpload
 from mtui.refhost import RefhostsFactory
 from mtui.utils import ensure_dir_exists, chdir
 from mtui.types import MD5Hash
@@ -21,6 +24,7 @@ from mtui.types.obs import RequestReviewID
 from mtui.utils import edit_text
 from mtui.messages import QadbReportCommentLengthWarning
 from mtui import updater
+from mtui.utils import ass_is, ass_isL
 
 try:
     from nose.tools import nottest
@@ -45,6 +49,19 @@ def testreport_svn_checkout(config, log, uri):
     with chdir(config.template_dir):
         # FIXME: use python module to perform svn checkout
         os.system('svn co {0}'.format(uri))
+
+class Scripts(object):
+    def __init__(self, scripts):
+        """
+        :type scripts: [L{Script}]
+        """
+        self.scripts = scripts
+
+    def run(self, targets):
+        ass_isL(targets, TargetI)
+
+        for x in self.scripts:
+            x.run(targets)
 
 class UpdateID(object):
     def __init__(self, id_, testreport_factory, testreport_svn_checkout):
@@ -119,7 +136,8 @@ class TestReport(object):
             type.
         """
 
-    def __init__(self, config, log, date):
+    def __init__(self, config, log, date, file_uploader = FileUpload,
+    cmd_runner = RunCommand, scripts_src_dir = None):
         """
         :type today: f :: L{datetime.date}
         """
@@ -127,6 +145,10 @@ class TestReport(object):
         self.log = log
         self._date = date
 
+        self.file_uploader = file_uploader
+        self.cmd_runner = cmd_runner
+
+        self._scripts_src_dir = scripts_src_dir
         self.directory = config.template_dir
 
         # Note: the default values here are unchanged from the previous
@@ -284,14 +306,20 @@ class TestReport(object):
     def get_downgrader(self):
         return updater.Downgrader[self.get_release()]
 
+    def scripts_src_dir(self):
+        if self._scripts_src_dir:
+            return self._scripts_src_dir
+
+        return join(self.config.datadir, 'scripts')
+
     def copy_scripts(self):
         if not self.path:
             raise RuntimeError("Called while missing path")
 
         # copy check_* and compare_* scripts to the template directory
         # TODO: do not override
-        src = join(self.config.datadir, 'scripts')
-        dst = join(dirname(self.path), 'scripts')
+        src = self.scripts_src_dir()
+        dst = self.scripts_wd()
 
         ignore = shutil.ignore_patterns('*.svn')
 
@@ -431,6 +459,15 @@ class TestReport(object):
         """
         return join(self.config.target_tempdir, str(self.id), *paths)
 
+    def scripts_wd(self, *paths):
+        """
+        :return: str path to the scripts dir joined with paths
+
+        Note this method does not create the directories as needed
+        because that's handled by L{TestReport.copy_scripts}
+        """
+        return join(self.report_wd(), *["scripts"] + list(paths))
+
     def patchinfo_url(self):
         return '/'.join([self.config.patchinfo_url, str(self.id)])
 
@@ -449,6 +486,19 @@ class TestReport(object):
             self.__class__.__name__,
             self.id
         )
+
+    def script_hooks(self, s):
+        """
+        :type s: L{Script} class
+        """
+
+        d = s.absolute_subdir(self)
+
+        return Scripts([
+            s(self, join(d, x), self.log, self.file_uploader, self.cmd_runner)
+            for r, _, fs in os.walk(d) if r == d
+            for x in fs
+        ])
 
 class TestsuiteComment(object):
     _max_comment_len = 100

mtui/utils.py
@@ -255,3 +255,16 @@ def requires_update(fn):
     wrap.__name__ = fn.__name__
     wrap.__doc__  = fn.__doc__
     return wrap
+
+def ass_is(x, class_, maybe_none = False):
+    if maybe_none and x is None:
+        return
+    assert isinstance(x, class_), "got {0!r}".format(x)
+
+if __debug__:
+    def ass_isL(xs, class_):
+        for x in xs:
+            ass_is(x, class_)
+else:
+    def ass_isL(_, __):
+        pass

commit c34d0a23391b0a2e5b1ee72524126e0c7ca923ec

Fix get/put mtui commands
broken since 9caba786433ad96c0efb14bb3eebbe54c1563e79

There was an error in acceptance testsuite that caused tests
for these commands to NOT be executed unless specified explicitly.

mtui/prompt.py
@@ -2405,7 +2405,7 @@ class CommandPrompt(cmd.Cmd):
 
             remote = self.target_tempdir(os.path.basename(filename))
 
-            FileUpload(self.targets, filename, remote).run()
+            FileUpload(self.targets.values(), filename, remote).run()
             self.log.info('uploaded {0} to {1}'.format(filename, remote))
 
     def complete_put(self, text, line, begidx, endidx):
@@ -2428,7 +2428,7 @@ class CommandPrompt(cmd.Cmd):
 
         local = self.downloads_wd(os.path.basename(args), filepath=True)
 
-        FileDownload(self.targets, args, local, True).run()
+        FileDownload(self.targets.values(), args, local, True).run()
         self.log.info('downloaded {0} to {1}'.format(args, local))
 
     def do_terms(self, args):