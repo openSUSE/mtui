openSUSE/mtui

commit d8bcb0becdd0991e1d87ed17948735cde7906d9d

TestReport#targets is now HostsGroup
HostsGroup supports more of the dict interface to limit the
extent of client code changes necessary *right now*.

selected_targets and enabled_targets from prompty.py are gone,
same as CommandPrompt#_hostsGroupFactory.

mtui/prompt.py
@@ -223,14 +223,8 @@ class CommandPrompt(cmd.Cmd):
 
         self.commandFactory(subcmd, args).run()
 
-    def _hostsGroupFactory(self):
-        """
-        :returns: L{HostsGroup} consisting of enabled hosts only
-        """
-        return HostsGroup(list(enabled_targets(self.targets).values()))
-
     def commandFactory(self, cmd, args=None):
-        hosts = self._hostsGroupFactory()
+        hosts = self.targets.select(enabled = True)
         return cmd(args, hosts, self.config, self.sys, self.log, self)
 
     def do_help(self, arg):
@@ -283,7 +277,7 @@ class CommandPrompt(cmd.Cmd):
                 raise AttributeError(str(x))
             else:
                 return log_exception(Exception, out.error)\
-                    (c.completer(self._hostsGroupFactory()))
+                    (c.completer(self.targets.select(enabled = True)))
         else:
             argparser = c.argparser(self.sys)
             clsdict = {
@@ -321,9 +315,9 @@ class CommandPrompt(cmd.Cmd):
             params = set([arg.strip() for arg in cmdline.split(',') if arg.strip()])
 
         if 'all' in tselected or tselected == set():
-            targets = enabled_targets(self.targets)
+            targets = self.targets.select(enabled = True)
         else:
-            targets = selected_targets(enabled_targets(self.targets), tselected)
+            targets = self.targets.select(tselected, enabled = True)
 
         return (targets, params)
 
@@ -460,7 +454,8 @@ class CommandPrompt(cmd.Cmd):
 
         targets, _ = self._parse_args(args, None)
         for tgt in targets:
-            self.targets[tgt].close() or self.targets.pop(tgt)
+            self.targets[tgt].close()
+            self.targets.pop(tgt)
 
     def complete_remove_host(self, text, line, begidx, endidx):
         return self.complete_hostlist_with_all(text, line, begidx, endidx)
@@ -2385,10 +2380,10 @@ class CommandPrompt(cmd.Cmd):
         return [i for i in list(self.targets) + ['all'] + appendix if i.startswith(text) and i not in line]
 
     def complete_enabled_hostlist(self, text, line, begidx, endidx, appendix=[]):
-        return [i for i in list(enabled_targets(self.targets)) + appendix if i.startswith(text) and i not in line]
+        return [i for i in list(self.targets.select(enabled = True)) + appendix if i.startswith(text) and i not in line]
 
     def complete_enabled_hostlist_with_all(self, text, line, begidx, endidx, appendix=[]):
-        return [i for i in list(enabled_targets(self.targets)) + ['all'] + appendix if i.startswith(text) and i not in line]
+        return [i for i in list(self.targets.select(enabled = True)) + ['all'] + appendix if i.startswith(text) and i not in line]
 
     def complete_packagelist(self, text, line, begidx, endidx, appendix=[]):
         return [i for i in self.metadata.get_package_list() if i.startswith(text) and i not in line]
@@ -2602,36 +2597,6 @@ class CompareScript(Script):
 
         logger(msg(argv, stdout, stderr, rc))
 
-
-def enabled_targets(targets):
-    """
-    :type targets: dict(hostname = L{Target})
-        where hostname = str
-
-    :returns: dict(hostname = L{Target})
-        where hostname = str
-    """
-    return dict(filter(
-      lambda x: x[1].state != 'disabled'
-    , targets.items()
-    ))
-
-def selected_targets(available, wanted):
-    """
-    :type available: dict(hostname = L{Target})
-        where hostname = str
-
-    :type wanted: [hostname]
-        where hostname = str
-
-    :returns: dict(hostname = L{Target})
-        where hostname = str
-    """
-    for x in wanted:
-        if not available.has_key(x):
-            out.warning('host %s not in database' % x)
-
-    return dict([x for x in available.items() if x[0] in wanted])
 
 def user_deprecation(out, msg):
     out.warning(msg)

mtui/target.py
@@ -72,18 +72,25 @@ class HostsGroup(object):
         """
         :param targets: list of L{Target}
         """
-        self.hosts = hosts
+        self.hosts = dict([(h.host, h) for h in hosts])
 
-    def select(self, hosts):
+    def select(self, hosts = [], enabled = None):
         if hosts == []:
+            if enabled:
+                return HostsGroup(filter(
+                  lambda h: h.state != 'disabled'
+                , self.hosts.values()
+                ))
             return self
 
-        available = [x.host for x in self.hosts]
         for x in hosts:
-            if not x in available:
+            if not x in self.hosts:
                 raise HostIsNotConnectedError(x)
 
-        return HostsGroup([x for x in self.hosts if x.host in hosts])
+        return HostsGroup([
+            h for hn, h in self.hosts.items()
+                if hn in hosts and ((not enabled) or h.state != 'disabled')
+        ])
 
     def unlock(self, *a, **kw):
         es = []
@@ -106,8 +113,30 @@ class HostsGroup(object):
     def __getitem__(self, x):
         return self.hosts[x]
 
+    def __iter__(self):
+        return self.hosts.__iter__()
+
+    def __len__(self):
+        return len(self.hosts)
+
+    def items(self):
+        return self.hosts.items()
+
+    def keys(self):
+        return self.hosts.keys()
+
+    def pop(self, *a, **kw):
+        return self.hosts.pop(*a, **kw)
+
+    def update(self, *a, **kw):
+        return self.hosts.update(*a, **kw)
+
+    def values(self):
+        return self.hosts.values()
+
     def names(self):
         return [x.hostname for x in self]
+
 
 class TargetLockedError(Exception):
     pass

mtui/template.py
@@ -13,6 +13,7 @@ from abc import abstractmethod
 from datetime import date
 import subprocess
 
+from mtui.target import HostsGroup
 from mtui.target import Target
 from mtui.target import TargetI
 from mtui.target import RunCommand
@@ -187,7 +188,7 @@ class TestReport(with_metaclass(ABCMeta, object)):
         :type systems: dict str -> str
         :param systems: hostname -> system
         """
-        self.targets = {}
+        self.targets = HostsGroup([])
         """
         :type  targets: dict(hostname = L{Target})
             where hostname = str

tests/test_hosts_group.py
@@ -14,8 +14,8 @@ def test_select_hosts():
     hg2 = hg.select(['a', 'c'])
     ok_(isinstance(hg2, HostsGroup))
     eq_(len(hg2.hosts), 2)
-    ok_(a in hg2.hosts)
-    ok_(c in hg2.hosts)
+    ok_(a in hg2.hosts.values())
+    ok_(c in hg2.hosts.values())
 
 def test_select_nohosts():
     a = Target('a', None, connect=False)

tests/test_target.py
@@ -253,30 +253,3 @@ class TestLockedTargets(object):
         except MyErr as e:
             eq_(str(e), m)
             self._check_locks(ts, False)
-
-
-def test_enabled_targets():
-    bar = TF("bar", state = 'enabled', connect = False)
-    qux = TF("qux", state = 'dryrun', connect = False)
-
-    xs = dict([(x.hostname, x) for x in [
-      TF("foo", state = 'disabled', connect = False)
-    , bar
-    , TF("baz", state = 'disabled', connect = False)
-    , qux
-    , TF("quux", state = 'disabled', connect = False)
-    ]])
-
-    eq_(prompt.enabled_targets(xs), {
-      bar.hostname: bar
-    , qux.hostname: qux
-    })
-
-def test_selected_targets():
-    eq_(
-      prompt.selected_targets(
-        dict(a = 1, b = 2, c = 3, d = 4)
-      , ['a', 'd']
-      )
-    , dict(a = 1, d = 4)
-    )

tests/test_template.py
@@ -521,6 +521,32 @@ def test_NullTestReport():
     assert_false(tr)
     eq_(tr.id, None)
 
+def test_select():
+    class TargetFake(Target):
+        def __init__(self, *args, **kw):
+            super(TargetFake, self).__init__(*args, connect = False, **kw)
+        def add_history(self, comment):
+            pass
+    tr = NullTestReport(ConfigFake(), LogFake(), date.today())
+    tr.targetFactory = TargetFake
+    tr.add_host('foo', 'fubar')
+    tr.add_host('bar', 'snafu')
+    tr.add_host('qux', 'snafubar')
+    tr.connect_targets()
+    ts = tr.targets
+    ts['qux'].state = 'disabled'
+
+    def s(s): return set(s.split())
+
+    selected = set(ts.select().keys())
+    eq_(selected, set('foo bar qux'.split()))
+    selected = set(ts.select('bar qux'.split()).keys())
+    eq_(selected, set('bar qux'.split()))
+    selected = set(ts.select(enabled = True).keys())
+    eq_(selected, set('foo bar'.split()))
+    selected = set(ts.select(['qux'], enabled = True).keys())
+    eq_(selected, set())
+
 def test_TC_edit_text():
     """
     Test L{TestsuiteComment.__str__} returns the new string after

commit 433a5f86c70ee98605b60bf7ffa286dab69c3262

fix CommandPrompt#do_run in presence of locked hosts
instead of bugfixing a questionable behavior (do_run filtered
out locked hosts), this change employs LockedTargets which means
do_run will now fail and dump the stacktrace if any of the requested
targets are locked.

broken in d8bcb0becdd0991e1d87ed17948735cde7906d9d, where
CommandPrompt#targets switched from dict to HostsGroup.

mtui/prompt.py
@@ -1080,13 +1080,7 @@ class CommandPrompt(cmd.Cmd):
 
         targets, command = self._parse_args(args, str)
 
-        for target in targets.keys():
-            lock = targets[target].locked()
-            if lock.locked and lock.comment and not lock.own():
-                self.log.critical('host %s is exclusively locked by %s (%s). skipping.' % (target, lock.user, lock.comment))
-                del targets[target]
-
-        if targets:
+        with LockedTargets(targets.values()):
             try:
                 targets.run(command)
             except KeyboardInterrupt: