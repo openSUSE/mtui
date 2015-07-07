openSUSE/mtui

commit 75e493679bcfeb4dc7c1288d7202cbd6dab2780f

do_testsuite_run: data retrieval separate from output

mtui/target/__init__.py
@@ -168,6 +168,10 @@ class HostsGroup(object):
         for hn in sorted(self.hosts.keys()):
             self.hosts[hn].report_testsuites(sink, arg)
 
+    def report_testsuite_results(self, sink, arg):
+        for hn in sorted(self.hosts.keys()):
+            self.hosts[hn].report_testsuite_results(sink, arg)
+
     ## dict interface
 
     def __getitem__(self, x):
@@ -620,6 +624,9 @@ class Target(object):
 
     def report_testsuites(self, sink, suitedir):
         return sink(self.hostname, self.system, self.listdir(suitedir))
+
+    def report_testsuite_results(self, sink, suitename):
+        return sink(self.hostname, t.lastexit(), t.lastout(), t.lasterr(), suitename)
 
 class Package(object):
 

mtui/prompt.py
@@ -1338,13 +1338,16 @@ class CommandPrompt(cmd.Cmd):
             self.log.info('testsuite run canceled')
             return
 
-        for target in targets:
-            self.println('{}:~> {}-testsuite [{}]'.format(target, name, targets[target].lastexit()))
-            self.println(targets[target].lastout())
-            if targets[target].lasterr():
-                self.println(targets[target].lasterr())
+        for hn, t in targets.items():
+            t.report_testsuite_results(self._do_testsuite_run, name)
 
         self.log.info('done')
+
+    def _do_testsuite_run(self, hostname, exit, stdout, stderr, suitename):
+        self.println('{}:~> {}-testsuite [{}]'.format(hostname, suitename, exit))
+        self.println(stdout)
+        if stderr:
+            self.println(stderr)
 
     def complete_testsuite_run(self, text, line, begidx, endidx):
         return self.complete_enabled_hostlist_with_all(text, line, begidx, endidx)

commit 03d34af1a0b0d0dd5bedba3817cc80490252d03c

fix do_testsuite_run
broken in 75e493679bcfeb4dc7c1288d7202cbd6dab2780f.

mtui/target/__init__.py
@@ -626,7 +626,7 @@ class Target(object):
         return sink(self.hostname, self.system, self.listdir(suitedir))
 
     def report_testsuite_results(self, sink, suitename):
-        return sink(self.hostname, t.lastexit(), t.lastout(), t.lasterr(), suitename)
+        return sink(self.hostname, self.lastexit(), self.lastout(), self.lasterr(), suitename)
 
 class Package(object):
 