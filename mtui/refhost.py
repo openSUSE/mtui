openSUSE/mtui

commit 65d8183fb0d12d195bd0465aae34add62ee32c06

CommandPrompt#do_search_hosts loses refhost attribute handling
the code goes to Attributes.from_search_hosts_query, next to
Attributes.from_testplatform.

mtui/refhost.py
@@ -116,6 +116,55 @@ class Attributes(object):
         return self.__bool__()
 
     @classmethod
+    def from_search_hosts_query(cls, q):
+        attrs = Attributes()
+
+        for _tag in q.split(' '):
+            tag = _tag.lower()
+            match = re.search('(\d+)\.(\d+)', tag)
+            if match:
+                attrs.major = match.group(1)
+                attrs.minor = match.group(2)
+            if tag in attrs.tags['products']:
+                attrs.product = tag
+            if tag in attrs.tags['archs']:
+                attrs.archs.append(tag)
+            if tag in attrs.tags['addons']:
+                attrs.addons.update(dict(tag = dict()))
+            if tag in attrs.tags['major']:
+                attrs.major = tag
+            if tag in attrs.tags['minor']:
+                attrs.minor = tag
+            if tag == 'kernel':
+                attrs.kernel = True
+            if tag == 'ltss':
+                attrs.ltss = True
+            if tag == 'minimal':
+                attrs.minimal = True
+            if tag == '!kernel':
+                attrs.kernel = False
+            if tag == '!ltss':
+                attrs.ltss = False
+            if tag == '!minimal':
+                attrs.minimal = False
+            if tag == 'xenu':
+                attrs.virtual.update(dict(mode = 'guest', hypervisor = 'xen'))
+            if tag == 'xen0':
+                attrs.virtual.update(dict(mode = 'host', hypervisor = 'xen'))
+            if tag == 'xen':
+                attrs.virtual.update(dict(hypervisor = 'xen'))
+            if tag == 'kvm':
+                attrs.virtual.update(dict(hypervisor = 'kvm'))
+            if tag == 'vmware':
+                attrs.virtual.update(dict(hypervisor = 'vmware'))
+            if tag == 'host':
+                attrs.virtual.update(dict(mode = 'host'))
+            if tag == 'guest':
+                attrs.virtual.update(dict(mode = 'guest'))
+
+        return attrs
+
+    @classmethod
     def from_testplatform(cls, testplatform, log):
         """
         Create a attribute object based on a testplatform string

mtui/prompt.py
@@ -341,7 +341,6 @@ class CommandPrompt(cmd.Cmd):
         if not args:
             self.parse_error(self.do_search_hosts, args)
 
-        attributes = Attributes()
         refhost = self._refhosts()
 
         if 'Testplatform:' in args:
@@ -356,54 +355,12 @@ class CommandPrompt(cmd.Cmd):
                 , self.log
                 ))
             except (ValueError, KeyError):
-                hosts = []
                 out.error('failed to parse Testplatform string')
+                return []
         elif refhost.get_host_attributes(args):
             hosts = [args]
         else:
-            for _tag in args.split(' '):
-                tag = _tag.lower()
-                match = re.search('(\d+)\.(\d+)', tag)
-                if match:
-                    attributes.major = match.group(1)
-                    attributes.minor = match.group(2)
-                if tag in attributes.tags['products']:
-                    attributes.product = tag
-                if tag in attributes.tags['archs']:
-                    attributes.archs.append(tag)
-                if tag in attributes.tags['addons']:
-                    attributes.addons.update({tag:{}})
-                if tag in attributes.tags['major']:
-                    attributes.major = tag
-                if tag in attributes.tags['minor']:
-                    attributes.minor = tag
-                if tag == 'kernel':
-                    attributes.kernel = True
-                if tag == 'ltss':
-                    attributes.ltss = True
-                if tag == 'minimal':
-                    attributes.minimal = True
-                if tag == '!kernel':
-                    attributes.kernel = False
-                if tag == '!ltss':
-                    attributes.ltss = False
-                if tag == '!minimal':
-                    attributes.minimal = False
-                if tag == 'xenu':
-                    attributes.virtual.update({'mode':'guest', 'hypervisor':'xen'})
-                if tag == 'xen0':
-                    attributes.virtual.update({'mode':'host', 'hypervisor':'xen'})
-                if tag == 'xen':
-                    attributes.virtual.update({'hypervisor':'xen'})
-                if tag == 'kvm':
-                    attributes.virtual.update({'hypervisor':'kvm'})
-                if tag == 'vmware':
-                    attributes.virtual.update({'hypervisor':'vmware'})
-                if tag == 'host':
-                    attributes.virtual.update({'mode':'host'})
-                if tag == 'guest':
-                    attributes.virtual.update({'mode':'guest'})
-
+            attributes = Attributes.from_search_hosts_query(args)
             hosts = refhost.search(attributes)
 
             # check if some tags were passed to the attributes object which has

commit adb51f53b62c8af67b5ff98e24e313943bba4a3d

fix addon handling in CommandPrompt#do_search_hosts
broken in 65d8183fb0d12d195bd0465aae34add62ee32c06.

mtui/refhost.py
@@ -130,7 +130,7 @@ class Attributes(object):
             if tag in attrs.tags['archs']:
                 attrs.archs.append(tag)
             if tag in attrs.tags['addons']:
-                attrs.addons.update(dict(tag = dict()))
+                attrs.addons.update({tag: dict()})
             if tag in attrs.tags['major']:
                 attrs.major = tag
             if tag in attrs.tags['minor']: