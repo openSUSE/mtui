9dfb11ad779c5fd4c845a7294b00ab87e6b92196
Roman Neuhauser <rneuhauser@suse.cz>
2015-07-14 12:17:37 +0200

    better tests for UpdateID#make_testreport
    the previous tests were plagued with indecent coupling to the CUT,
    hampering any attempts at refactoring UpdateID or cooperating classes.
    
    test_UID_mtr__copy_scripts_src_missing should've been called
    "peeping tom", it was testing a small part of TestReport#read
    while cementing a bunch of CUT implementation details.

--- tests/test_make_testreport.py
+++ tests/test_make_testreport.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
+import errno
 import os
 import shutil
 from os.path import dirname
@@ -11,25 +12,12 @@ from tempfile import mkdtemp
 
 from tests.utils import ConfigFake
 from tests.utils import LogFake
-from tests.utils import get_nonexistent_path
 from tests.utils import unused
 
 from nose.tools import ok_, eq_
 
-from mtui.template import TestReport
-from mtui.template import SwampTestReport
-from mtui.template import SwampUpdateID
+from mtui.template import UpdateID
 from mtui.template import _TemplateIOError
-
-
-class TestReportSVNCheckoutFake(object):
-  def __init__(self, template_path):
-    self.path = template_path
-
-  def __call__(self, *a, **kw):
-    os.makedirs(dirname(self.path))
-    with open(self.path, "w") as f:
-      f.write("unused")
 
 
 def test_UID_mtr_success():
@@ -41,29 +29,37 @@ def test_UID_mtr_success():
   2. passes config and log objects on to the testreport instance
   """
 
+  class TestReportFake:
+    def __init__(tr, *a, **kw):
+      tr.init_args = a
+      tr.init_kwargs = kw
+      tr.read_calls = 0
+      tr.read_args = []
+      tr.read_kwargs = []
+    def read(tr, *a, **kw):
+      tr.read_calls += 1
+      tr.read_args += [a]
+      tr.read_kwargs += [kw]
+
   d = mkdtemp()
 
   try:
     c = ConfigFake()
     c.template_dir = d
     c.svn_path = unused
+    l = LogFake()
 
-    u = SwampUpdateID('82407e2d7113cfde72f65d81e4ffee61')
+    u = UpdateID('82407e2d7113cfde72f65d81e4ffee61', TestReportFake, unused)
     u.config = c
-    u.log = LogFake()
-    class TestReportFake(SwampTestReport):
-      def _parse(self, file_):
-        pass
-
-    u.testreport_factory = TestReportFake
-
-    TestReportSVNCheckoutFake(u._template_path())()
-
+    u.log = l
     tr = u.make_testreport()
 
     ok_(isinstance(tr, TestReportFake))
-    eq_(u.config, tr.config)
-    eq_(u.log, tr.log)
+    eq_(c, tr.init_args[0])
+    eq_(l, tr.init_args[1])
+    eq_(tr.read_calls, 1)
+    eq_(tr.read_args[0], (("%s/82407e2d7113cfde72f65d81e4ffee61/log" % d),))
+    eq_(tr.read_kwargs[0], dict())
   finally:
     shutil.rmtree(d)
 
@@ -73,51 +69,119 @@ def test_UID_mtr_with_checkout():
   Test UpdateID.make_testreport does vcs_checkout if can't read the
   report
   """
+
+  class TestReportFake:
+    def __init__(tr, *a, **kw):
+      tr.read_calls = 0
+      tr.init_args = a
+      tr.init_kwargs = kw
+    def read(tr, *a, **kw):
+      tr.read_calls += 1
+      if tr.read_calls == 1:
+        e = _TemplateIOError()
+        e.errno = errno.ENOENT
+        raise e
+
+  class CheckoutFake:
+    def __init__(co):
+      co.calls = 0
+      co.args = []
+      co.kwargs = []
+    def __call__(co, *a, **kw):
+      co.calls += 1
+      co.args += [a]
+      co.kwargs += [kw]
+
   d = mkdtemp()
 
   try:
     c = ConfigFake()
     c.template_dir = d
     c.svn_path = 'svnpath'
+    l = LogFake()
+    co = CheckoutFake()
 
-    u = SwampUpdateID('82407e2d7113cfde72f65d81e4ffee61')
+    u = UpdateID('82407e2d7113cfde72f65d81e4ffee61', TestReportFake, co)
     u.config = c
-    u.log = LogFake()
-    u._vcs_checkout = TestReportSVNCheckoutFake(u._template_path())
-
-    class TestReportFake(SwampTestReport):
-      def _parse(self, file_):
-        pass
-    u.testreport_factory = TestReportFake
+    u.log = l
 
     tr = u.make_testreport()
 
     ok_(isinstance(tr, TestReportFake))
-    eq_(u.config, tr.config)
-    eq_(u.log, tr.log)
+    eq_(c, tr.init_args[0])
+    eq_(l, tr.init_args[1])
+    eq_(tr.read_calls, 2)
+    eq_(co.calls, 1)
+    eq_(co.args, [(c, l, '%s/%s' % (c.svn_path, '82407e2d7113cfde72f65d81e4ffee61'))])
   finally:
     shutil.rmtree(d)
 
 
-def test_UID_mtr_failing_checkout():
+def test_UID_mtr_failing_after_checkout():
   """
-  Test UpdateID.make_testreport raises
+  Test UpdateID.make_testreport in face of silent checkout failure
   """
+
+  # UpdateID gets an instance of this TestReportFake class since the test
+  # needs something to interrogate after the make_testreport failure.
+  class TestReportFake:
+    def __init__(tr):
+      tr.init_calls = 0
+      tr.init_args = []
+      tr.init_kwargs = []
+      tr.read_calls = 0
+      tr.read_args = []
+      tr.read_kwargs = []
+
+    def __call__(tr, *a, **kw):
+      tr.init_calls += 1
+      tr.init_args = [a]
+      tr.init_kwargs = [kw]
+      return tr
+
+    def read(tr, *a, **kw):
+      tr.read_calls += 1
+      tr.read_args += [a]
+      tr.read_kwargs += [kw]
+      e = _TemplateIOError()
+      e.errno = errno.ENOENT
+      raise e
+
+
+  class CheckoutFake:
+    def __init__(co):
+      co.calls = 0
+      co.args = []
+      co.kwargs = []
+    def __call__(co, *a, **kw):
+      co.calls += 1
+      co.args += [a]
+      co.kwargs += [kw]
+
+  tr = TestReportFake()
+  co = CheckoutFake()
   d = mkdtemp()
 
   try:
     c = ConfigFake()
     c.template_dir = d
     c.svn_path = 'svnpath'
+    l = LogFake()
 
-    u = SwampUpdateID('82407e2d7113cfde72f65d81e4ffee61')
+    u = UpdateID('82407e2d7113cfde72f65d81e4ffee61', tr, co)
     u.config = c
-    u.log = LogFake()
-    u._vcs_checkout = lambda *a, **kw: unused
+    u.log = l
 
     tr = u.make_testreport()
-  except _TemplateIOError:
-    pass
+  except _TemplateIOError as e:
+    eq_(e.errno, errno.ENOENT)
+    eq_(co.calls, 1)
+    eq_(co.args[0], (c, l, 'svnpath/82407e2d7113cfde72f65d81e4ffee61'))
+    eq_(tr.init_calls, 1)
+    eq_(tr.init_args, [(c, l)])
+    eq_(tr.read_calls, 2)
+    for i in (0, 1):
+      eq_(tr.read_args[i], (('%s/82407e2d7113cfde72f65d81e4ffee61/log' % d),))
   else:
     ok_(False, "_TemplateIOError expected to be raised")
   finally:
@@ -128,54 +192,58 @@ def test_UID_mtr_other_ioerror():
   """
   Test UpdateID.make_testreport raises
   """
+  # UpdateID gets an instance of this TestReportFake class since the test
+  # needs something to interrogate after the make_testreport failure.
+  class TestReportFake:
+    def __init__(tr):
+      tr.init_calls = 0
+      tr.init_args = []
+      tr.init_kwargs = []
+      tr.read_calls = 0
+      tr.read_args = []
+      tr.read_kwargs = []
 
+    def __call__(tr, *a, **kw):
+      tr.init_calls += 1
+      tr.init_args = [a]
+      tr.init_kwargs = [kw]
+      return tr
+
+    def read(tr, *a, **kw):
+      tr.read_calls += 1
+      tr.read_args += [a]
+      tr.read_kwargs += [kw]
+      if tr.read_calls == 1:
+        e = _TemplateIOError()
+        e.errno = errno.EACCES
+        raise e
+
+  def checkout(*a, **kw):
+    ok_(False, "shouldn't try to perform checkout")
+
+  tr = TestReportFake()
   d = mkdtemp()
 
   try:
     c = ConfigFake()
     c.template_dir = d
     c.svn_path = 'svnpath'
+    l = LogFake()
 
-    u = SwampUpdateID('82407e2d7113cfde72f65d81e4ffee61')
+    u = UpdateID('82407e2d7113cfde72f65d81e4ffee61', tr, checkout)
     u.config = c
-    u.log = LogFake()
-    u._vcs_checkout = lambda *a, **kw: \
-      ok_(False, "shouldn't try to perform checkout")
-
-    TestReportSVNCheckoutFake(u._template_path())()
-    os.chmod(u._template_path(), 0)
+    u.log = l
 
     try:
       tr = u.make_testreport()
     except _TemplateIOError as e:
-      pass
+      eq_(e.errno, errno.EACCES)
+      eq_(tr.init_calls, 1)
+      eq_(tr.init_args, [(c, l)])
+      eq_(tr.read_calls, 1)
+      eq_(tr.read_args, [(('%s/82407e2d7113cfde72f65d81e4ffee61/log' % d),)])
     else:
       ok_(False, "_TemplateIOError expected to be raised")
-  finally:
-    shutil.rmtree(d)
-
-
-def test_UID_mtr__copy_scripts_src_missing():
-  """
-  Test make_testreport() when scripts are missing in datadir
-  """
-
-  d = mkdtemp()
-
-  try:
-    c = ConfigFake()
-    c.template_dir = d
-    c.svn_path = 'svnpath'
-    c.datadir = get_nonexistent_path()
-
-    u = SwampUpdateID('82407e2d7113cfde72f65d81e4ffee61')
-    u.config = c
-    u.log = LogFake()
-    u._vcs_checkout = TestReportSVNCheckoutFake(u._template_path())
-
-    tr = u.make_testreport()
-    eq_(tr.log.errors[-1], 'copy scripts manually')
-    ok_(isinstance(tr, TestReport))
   finally:
     shutil.rmtree(d)
 
