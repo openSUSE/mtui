openSUSE/mtui

commit b10e35bb541bd7a8c82303f3693486bf9f0d37fd

curb KeyError (ab)use
esp. CommandPrompt#__getattr__ is much more readable now.

mtui/prompt.py
@@ -199,10 +199,11 @@ class CommandPrompt(cmd.Cmd):
     def onecmd(self, line):
         # FIXME: see L{CommandPrompt.__getattr__}
         cmd_, arg, line = self.parseline(line)
-        try:
-            subcmd = self.commands[cmd_]
-        except KeyError:
+
+        if not self.commands.has_key(cmd_):
             return cmd.Cmd.onecmd(self, line)
+
+        subcmd = self.commands[cmd_]
 
         try:
             args = subcmd.parse_args(arg, self.sys)
@@ -216,12 +217,11 @@ class CommandPrompt(cmd.Cmd):
 
     def do_help(self, arg):
         # FIXME: see L{CommandPrompt.__getattr__}
-        try:
-            cmd_ = self.commands[arg]
-        except KeyError:
+        if not self.commands.has_key(arg):
             return cmd.Cmd.do_help(self, arg)
-        else:
-            cmd_.argparser(self.sys).print_help()
+
+        cmd_ = self.commands[arg]
+        cmd_.argparser(self.sys).print_help()
 
     def get_names(self):
         names = cmd.Cmd.get_names(self)
@@ -249,28 +249,23 @@ class CommandPrompt(cmd.Cmd):
         # 2. fix L{Command} so that it provides __call__ and the
         #    Command instance can be returned for "do_<cmd>", therefore
         #    removing the need to override both do_help and onecmd.
-        try:
-            y = x
-            if isinstance(y, str):
-                y = y.replace("do_","")
+
+        y = x.replace("do_","")
+        if self.commands.has_key(y):
             c = self.commands[y]
-        except KeyError:
-            try:
-                y = x
-                if isinstance(y, str):
-                    y = y.replace("complete_","")
-                c = self.commands[y]
-            except KeyError:
-                raise AttributeError(str(x))
-            else:
-                return log_exception(Exception, self.log.error)\
-                    (c.completer(self.targets.select(enabled = True)))
-        else:
             argparser = c.argparser(self.sys)
             clsdict = {
                 '__doc__': argparser.format_help()
             }
             return type(x, (object,), clsdict)
+
+        y = x.replace("complete_","")
+        if self.commands.has_key(y):
+            c = self.commands[y]
+            return log_exception(Exception, self.log.error) \
+                (c.completer(self.targets.select(enabled = True)))
+
+        raise AttributeError(str(x))
     # }}}
 
     def emptyline(self):
@@ -390,15 +385,16 @@ class CommandPrompt(cmd.Cmd):
         return [item for sublist in attributes.tags.values() for item in sublist if item.startswith(text) and item not in line]
 
     def connect_system_if_unconnected(self, hostname, system):
-        try:
+        if self.targets[hostname].has_key(hostname):
             self.log.warning('already connected to {0}. skipping.'.format(
                 self.targets[hostname].hostname
             ))
-        except KeyError:
-            self.targets[hostname] = Target(self.config, hostname, system, self.metadata.get_package_list(), logger = self.log)
+            return
 
-            if self.metadata:
-                self.metadata.systems[hostname] = system
+        self.targets[hostname] = Target(self.config, hostname, system, self.metadata.get_package_list(), logger = self.log)
+
+        if self.metadata:
+            self.metadata.systems[hostname] = system
 
     def do_add_host(self, args):
         """

mtui/commands.py
@@ -258,12 +258,7 @@ class ReportBug(Command):
     stable = '3.0b2'
 
     def __init__(self, *a, **kw):
-        try:
-            self.popen = kw['popen']
-        except KeyError:
-            self.popen = Popen
-        else:
-            del kw['popen']
+        self.popen = kw.pop('popen', Popen)
 
         super(ReportBug, self).__init__(*a, **kw)
 

mtui/target/downgrade.py
@@ -75,19 +75,11 @@ class Downgrade(object):
           if match:
             name = match.group(1)
             version = match.group(2)
-            try:
-              release[name].append(version)
-            except KeyError:
-              release[name] = []
-              release[name].append(version)
+            release.setdefault(name, []).append(version)
 
         for name in release:
           version = sorted(release[name], key=RPMVersion, reverse=True)[0]
-          try:
-            versions[hn].update({name:version})
-          except KeyError:
-            versions[hn] = {}
-            versions[hn].update({name:version})
+          versions.setdefault(hn, dict()).update({name: version})
 
       for command in self.pre_commands:
         self.targets.run(command)

mtui/updater.py
@@ -35,12 +35,11 @@ class ZypperUpToSLE11Update(ZypperUpdate):
     def __init__(self, *a, **kw):
         super(ZypperUpToSLE11Update, self).__init__(*a, **kw)
 
-        try:
-            patch = self.patches['sat']
-        except KeyError:
+        if not self.patches.has_key('sat'):
             self.log.critical('required SAT patch number for zypper update not found')
             return
 
+        patch = self.patches['sat']
         self.commands = [
             'export LANG=',
             'zypper lr -puU',
@@ -70,12 +69,11 @@ class openSuseUpdate(Update):
     def __init__(self, *a, **kw):
         super(openSuseUpdate, self).__init__(*a, **kw)
 
-        try:
-            patch = self.patches['sat']
-        except KeyError:
+        if not self.patches.has_key('sat'):
             self.log.critical('required SAT patch number for zypper update not found')
             return
 
+        patch = self.patches['sat']
         self.commands = [
             'export LANG=',
             'zypper -v lr -puU',
@@ -87,12 +85,11 @@ class OldZypperUpdate(Update):
     def __init__(self, *a, **kw):
         super(OldZypperUpdate, self).__init__(*a, **kw)
 
-        try:
-            patch = self.patches['zypp']
-        except KeyError:
+        if not self.patches.has_key('zypp'):
             self.log.critical('required ZYPP patch number for zypper update not found')
             return
 
+        patch = self.patches['zypp']
         self.commands = [
             'export LANG=',
             'zypper sl',
@@ -105,12 +102,11 @@ class OnlineUpdate(Update):
     def __init__(self, *a, **kw):
         super(OnlineUpdate, self).__init__(*a, **kw)
 
-        try:
-            patch = self.patches['you']
-        except KeyError:
+        if not self.patches.has_key('you'):
             self.log.critical('required YOU patch number for online_update update not found')
             return
 
+        patch = self.patches['you']
         self.commands = [
             'export LANG=',
             'find /var/lib/YaST2/you/ -name patch-%s' % patch,
@@ -122,12 +118,11 @@ class RugUpdate(Update):
     def __init__(self, *a, **kw):
         super(RugUpdate, self).__init__(*a, **kw)
 
-        try:
-            patch = self.patches['you']
-        except KeyError:
+        if not self.patches.has_key('you'):
             self.log.critical('required YOU patch number for rug update not found')
             return
 
+        patch = self.patches['you']
         self.commands = [
             'export LANG=',
             'rug sl',
@@ -242,12 +237,11 @@ class OldZypperDowngrade(Downgrade):
     def __init__(self, *a, **kw):
         super(OldZypperDowngrade, self).__init__(*a, **kw)
 
-        try:
-            patch = self.patches['zypp']
-        except KeyError:
+        if not self.patches.has_key('zypp'):
             self.log.critical('required ZYPP patch number for zypper downgrade not found')
             return
 
+        patch = self.patches['zypp']
         self.list_command = 'zypper se --match-exact -t package %s | grep -v "^[iv] |[[:space:]]\+|" | grep ^[iv] | sed "s, ,,g" | awk -F "|" \'{ print $4,"=",$5 }\'' % ' '.join(self.packages)
         self.install_command = 'rpm -q %s &>/dev/null && (line=$(zypper se --match-exact -t package %s | grep %s); repo=$(zypper sl | grep "$(echo $line | cut -d \| -f 2)" | cut -d \| -f 6); if expr match "$repo" ".*/DVD1.*" &>/dev/null; then subdir="suse"; else subdir="rpm"; fi; url=$(echo -n "$repo/$subdir" | sed -e "s, ,,g" ; echo $line | awk \'{ print "/"$11"/"$7"-"$9"."$11".rpm" }\'); package=$(basename $url); if [ ! -z "$repo" ]; then wget -q $url; rpm -Uhv --nodeps --oldpackage $package; rm $package; fi)'
 

mtui/utils.py
@@ -290,12 +290,7 @@ def get_release(systems):
 
 class DictWithInjections(dict):
     def __init__(self, *args, **kw):
-        try:
-            self.key_error = kw['key_error']
-        except KeyError:
-            self.key_error = KeyError
-        else:
-            del kw['key_error']
+        self.key_error = kw.pop('key_error', KeyError)
 
         super(DictWithInjections, self).__init__(*args, **kw)
 

commit 4247cf47898363aaeb148695b19deb6bb862c344

fix add_host / mtui -s, add HostsGroup#has_key
broken in b10e35bb541bd7a8c82303f3693486bf9f0d37fd.

mtui/prompt.py
@@ -385,7 +385,7 @@ class CommandPrompt(cmd.Cmd):
         return [item for sublist in attributes.tags.values() for item in sublist if item.startswith(text) and item not in line]
 
     def connect_system_if_unconnected(self, hostname, system):
-        if self.targets[hostname].has_key(hostname):
+        if self.targets.has_key(hostname):
             self.log.warning('already connected to {0}. skipping.'.format(
                 self.targets[hostname].hostname
             ))

mtui/target/__init__.py
@@ -185,6 +185,9 @@ class HostsGroup(object):
     def copy(self):
         return HostsGroup(self.hosts.values())
 
+    def has_key(self, *a, **kw):
+        return self.hosts.has_key(*a, **kw)
+
     def items(self):
         return self.hosts.items()
 