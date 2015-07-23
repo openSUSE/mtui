7e6680bdfa662fbd6ba93e20c319adace570728d
Ondřej Súkup <osukup@suse.com>
2015-07-23 18:34:46 +0200

    check_new_depencencies.sh, compare_new_dependencies.sh anew
    the old code had two problems:
    
    * it disabled all repo's with TESTING in name, causing problems for
      updates without proper rep-clean support such as SUSE-Cloud.
    
      let's say you need to test something rep-clean.sh does not know or
      care about.  you'll have to add the repository with candidate packages
      by hand, and it needs to be named "TESTING-$ISSUEID".  then `update`
      in mtui will attempt to reset repositories into a known state for you
      with `rep-clean.sh -z -i $ISSUEID` and fail, leaving you without the
      repo you snuck in (`-z` works well, it's the -`i $ISSUEID` part that
      fails).
    
    * it was inefficient and worked in an obscure and roundabout manner.
    
      it was *in theory* a more thorough approach (compare lists of all
      installed packages that *should* be from the TESTING repositories),
      but with shared refhosts all bets are off, and you never know if
      a result you've got isn't tainted by a concurrent work of another
      tester.
    
    the new code does not touch any repositories.  dependencies for each
    package under test are collected with `rpm -q --requires`, each line
    tagged with the name of the dependent package.  since this observes
    package metadata instead of actual effects, there's a possibility
    a rogue dependency might sneak in, maybe through malevolent or
    incompetent code in a %post hook or similar.  there's a concensus
    at least among osukup@, rneuhauser@ and tchvatal@ that this should
    not be a concern for this particular script.

--- helper/check_new_dependencies.sh
+++ helper/check_new_dependencies.sh
@@ -5,10 +5,7 @@ export LC_ALL=C
 help () {
    cat <<EOF
 
-usage: $0 -p <path-to-filelist> [id]
-
-where id is either \$md5sum or (openSUSE|SUSE):Maintenance:\$issue:\$request
-
+usage: $0 -p <path-to-package-list>
 $0 dumps all rpm package versions
 
 EOF
@@ -29,13 +26,13 @@ while true; do
    esac
 done
 
-id="$1"
-
 if [ -n "$help" -o -z "$plist" ]; then
    help
    exit 0
 fi
 
+# this needs to be sorted for compare_new_dependencies.sh
+# to work properly
 list=$(
     sed -n '/\.delta\.\(log\|info\|rpm\)/! { /\.rpm$/p; }' $plist | while read p
     do
@@ -44,25 +41,47 @@ list=$(
     done | sort -u | xargs
 )
 
-echo "list: $list"
-if grep -Eq "VERSION = (11|12)" /etc/SuSE-release; then
-    for repo in $(zypper lr -s | awk -F\| '{ print $2 }'); do
-        if [[ "$repo" =~ "TESTING" ]]; then
-            zypper -n mr -d $repo >/dev/null 2>&1
-        elif [[ "$repo" =~ "UPDATE" ]]; then
-            zypper -n mr -e $repo >/dev/null 2>&1
-        fi
-    done
-    zypper -n se -t package -is 2>/dev/null | egrep '(TESTING|System)' | awk -F\| '{gsub(/[ \t]+/,""); print $2"-"$4 }' | sort -u
-elif grep -q "VERSION = 10" /etc/SuSE-release; then
-    for repo in /var/lib/zypp/db/sources/*; do
-        if grep -q TESTING $repo; then
-            sed -i -e 's,<enabled>.*</enabled>,<enabled>false</enabled>,g' $repo
-	elif grep -q UPDATE $repo; then
-            sed -i -e 's,<enabled>.*</enabled>,<enabled>true</enabled>,g' $repo
-        fi
-    done
-    zypper -n se -t package -i 2>/dev/null | egrep '(TESTING|System)' | awk -F\| '{gsub(/[ \t]+/,""); print $4"-"$5 }' | sort -u
-else
-    rpm -qa --queryformat '%{NAME}-%{VERSION}-%{RELEASE}\n' | sort -u
-fi
+# `rpm -q --requires` outputs lines in these formats:
+# * package dependency
+# * shared lib dependency
+# * path
+# * capability line
+# * error message about uninstalled package
+#
+# examples:
+#   /usr/bin/python
+#   python-m2crypto > 0.19
+#   python-urlgrabber
+#   config(osc) = 0.152.0-1.1
+#   libdl.so.2(GLIBC_2.2.5)(64bit)
+#   libreadline.so.6()(64bit)
+#   package fubar is not installed
+#
+# output is *mostly* sorted and without duplicates, but see this:
+#
+#  edna:~ # rpm -q --requires gawk
+#  /bin/sh
+#  /bin/sh
+#  /bin/sh
+#  /bin/sh
+#  info
+#  libc.so.6()(64bit)
+#  [...]
+#  rpmlib(PayloadFilesHavePrefix) <= 4.0-1
+#  update-alternatives
+#  rpmlib(PayloadIsLzma) <= 4.4.6-1
+
+for p in $list; do
+  rpm -q --requires $p \
+  | sort -u \
+  | sed "
+      /is not installed/d
+      # a line with spaces should be a line in the form CAP OP VER,
+      # we're only doing loose comparisons so strip the latter two
+      s/ .*//
+      # label each dependency with the dependent package so
+      # compare_new_dependencies.sh knows what it's looking at and
+      # can present to user which package is affected by each change.
+      s/^/$p: /
+    "
+done
--- helper/compare_new_dependencies.sh
+++ helper/compare_new_dependencies.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
+export LC_ALL=C
+
 progname=${0##*/}
 
 if [ -z "$1" -o -z "$2" ]; then
-   echo "usage: $progname <logfile before the update> <logfile after the update>"
+   echo "usage: $progname <dependencies before the update> <dependencies after the update>"
    exit 2
 fi
 
-list=$(grep "list:" "$1" | sed 's,list: ,,g')
-#changed=$(diff -Nur "$1" "$2" | grep "^\+[[:alpha:]]" | cut -d: -f1 | sed 's,^+,,g')
-changed=$(diff -Nur "$1" "$2" | grep "^\+[[:alpha:]]" | sed 's,^+,,g')
 
-for package in $changed; do
-   package=$(echo $package | sed 's,-[^-]*-[^-]*$,,g')
-   if ! [[ "$list " =~ "$package " ]]; then
-      new="$new $package"
-   fi
-done
-
-if [ -z "$new" ]; then
-   echo "no external packages affected"
-   exit 0
-else
-   echo "ERROR: affected external packages: $new" >&2
-   exit 1
+if cmp -s "$1" "$2"; then
+  echo "no deps changed"
+  exit 0
 fi
+
+{
+  echo "ERROR: dropped dependencies:"
+  comm -23 "$1" "$2"
+  echo "ERROR: added dependencies:"
+  comm -13 "$1" "$2"
+  exit 1
+} >&2
 
