openSUSE/mtui

commit 26153694cc166f888aaee2c7e2b8bc5cd6189fc6

restore sanity around utils.edit_text
edit_text accepts a string and is supposed to put it in a tempfile,
let the user edit it, read it back again and return the result.
however, it was written such that a failure in any step was ignored
and the result was potentially garbled.

TestsuiteComment, one of the edit_text users, was a fine piece of
absurdist art in its own right.

mtui/prompt.py
@@ -981,7 +981,12 @@ class CommandPrompt(cmd.Cmd):
             fields.insert(0, 'package: %s' % package)
             fields.insert(0, 'summary: %s' % summary)
 
-            edited = edit_text('\n'.join(fields))
+            try:
+                edited = edit_text('\n'.join(fields))
+            except subprocess.CalledProcessError as e:
+                self.log.error("editor failed: %s" % e)
+                self.log.debug(format_exc())
+                return
 
             if edited == '\n'.join(fields):
                 out.warning('testcase was not modified. not uploading.')
@@ -1051,7 +1056,12 @@ class CommandPrompt(cmd.Cmd):
             for field in fields:
                 template.append('%s: %s' % (field, testcase[field]))
 
-            edited = edit_text('\n'.join(template))
+            try:
+                edited = edit_text('\n'.join(template))
+            except subprocess.CalledProcessError as e:
+                self.log.error("editor failed: %s" % e)
+                self.log.debug(format_exc())
+                return
 
             if edited == '\n'.join(template):
                 out.warning('testcase was not modified. not uploading.')
@@ -1385,7 +1395,15 @@ class CommandPrompt(cmd.Cmd):
         username = config.session_user
 
         comment = self.metadata.get_testsuite_comment(name)
-        comment.edit_text()
+        try:
+            comment = edit_text(comment)
+        except subprocess.CalledProcessError as e:
+            self.log.error("editor failed: %s" % e)
+            self.log.debug(format_exc())
+            return
+
+        if len(s) > self._max_comment_len:
+            self.log.warning(QadbReportCommentLengthWarning())
 
         out.info('please specify rd-qa NIS password')
         password = getpass.getpass()

mtui/template.py
@@ -27,7 +27,6 @@ from mtui import utils
 from mtui.utils import ensure_dir_exists, chdir
 from mtui.types import MD5Hash
 from mtui.types.obs import RequestReviewID
-from mtui.utils import edit_text
 from mtui.five import with_metaclass
 from mtui.messages import QadbReportCommentLengthWarning
 from mtui.messages import FailedToDownloadSrcRPMError
@@ -523,12 +522,10 @@ class TestReport(with_metaclass(ABCMeta, object)):
         return self.report_wd('packages-list.txt', filepath = True)
 
     def get_testsuite_comment(self, testsuite):
-        return TestsuiteComment(
-            self.log,
-            "{0} {1}".format(self._type, self.id),
+        return 'testing %s on %s on %s' % (
             testsuite,
-            self._date.today(),
-            text_editor = edit_text
+            "%s %s" % (self._type, self.id),
+            self._date.today().strftime('%d/%m/%y'),
         )
 
     def __repr__(self):
@@ -618,42 +615,6 @@ class TestReport(with_metaclass(ABCMeta, object)):
 
         return by_hosts_pkg
 
-class TestsuiteComment(object):
-    _max_comment_len = 100
-
-    def __init__(self, log, update_id, testsuite, date, text_editor = None):
-        """
-        :type update_id: str
-        :type testsuite: str or None
-        :type date: L{datetime.date}
-        :type text_editor: f :: str -> str
-        """
-        self.update_id = update_id
-        self.log = log
-        self.date = date
-        self.testsuite = testsuite
-
-        self._user_str = None
-        self._text_editor = text_editor
-
-    def _to_str(self):
-        if self._user_str:
-            return self._user_str
-
-        return 'testing {2} on {0} on {1}'.format(
-            self.update_id,
-            self.date.strftime('%d/%m/%y'),
-            self.testsuite
-        )
-
-    def __str__(self):
-        xs = self._to_str()
-        if len(xs) > self._max_comment_len:
-            self.log.warning(QadbReportCommentLengthWarning())
-        return xs
-
-    def edit_text(self):
-        self._user_str = self._text_editor(str(self))
 
 class NullTestReport(TestReport):
     _type = "No"

tests/test_template.py
@@ -19,7 +19,6 @@ from mtui.template import TestReport
 from mtui.template import NullTestReport
 from mtui.template import SwampTestReport
 from mtui.template import OBSTestReport
-from mtui.template import TestsuiteComment
 from mtui.template import SwampUpdateID
 from mtui.template import _TemplateIOError
 from mtui.template import QadbReportCommentLengthWarning
@@ -500,7 +499,6 @@ def test_swamp_get_testsuite_comment():
     tr = TRF(SwampTestReport, date_ = date)
     tr.md5 = MD5Hash('8c60b7480fc521d7eeb322955b387165')
     comment = tr.get_testsuite_comment("tsuite")
-    ok_(isinstance(comment, TestsuiteComment))
     eq_(str(comment), "testing tsuite on SWAMP {0} on {1}".format(
         tr.md5,
         date.today().strftime("%d/%m/%y"),
@@ -510,7 +508,6 @@ def test_obs_get_testsuite_comment():
     tr = TRF(OBSTestReport, date_ = date)
     tr.rrid = RequestReviewID("SUSE:Maintenance:1:1")
     comment = tr.get_testsuite_comment("tsuite")
-    ok_(isinstance(comment, TestsuiteComment))
     eq_(str(comment), "testing tsuite on OBS {0} on {1}".format(
         tr.rrid,
         date.today().strftime("%d/%m/%y"),
@@ -546,39 +543,6 @@ def test_select():
     eq_(selected, set('foo bar'.split()))
     selected = set(ts.select(['qux'], enabled = True).keys())
     eq_(selected, set())
-
-def test_TC_edit_text():
-    """
-    Test L{TestsuiteComment.__str__} returns the new string after
-    L{TestsuiteComment.edit_text} was executed.
-    """
-    tc = TestsuiteComment(LogFake(), "foo", "bar", date.today(),
-        text_editor = lambda _: "meh"
-    )
-    ok_(str(tc).startswith("testing bar on foo on "))
-    tc.edit_text()
-    eq_(str(tc), "meh")
-
-def test_TC_str_warning():
-    """
-    Test L{TestsuiteComment.__str__} issues the warning when comment too
-    long
-    """
-    tc = TestsuiteComment(LogFake(), unused, unused, date.today(),
-        lambda _: "-" * TestsuiteComment._max_comment_len
-    )
-
-    tc.edit_text()
-    str(tc)
-    eq_(tc.log.warnings, [])
-
-    tc = TestsuiteComment(LogFake(), unused, unused, date.today(),
-        lambda _: "-" * (TestsuiteComment._max_comment_len+1)
-    )
-    tc.edit_text()
-    eq_(tc.log.warnings, [])
-    str(tc)
-    eq_(tc.log.warnings.pop(), QadbReportCommentLengthWarning())
 
 def test_get_release():
     cases = [

commit 1535d8440c6fc06536822860e4ac5beb0e81c8ec

fix NameError, AttributeError in CommandPrompt#do_testsuite_submit
three bugs in a single change, ffs!

fixes bsc#939695
broken in 26153694cc166f888aaee2c7e2b8bc5cd6189fc6

mtui/prompt.py
@@ -1402,7 +1402,7 @@ class CommandPrompt(cmd.Cmd):
             self.log.debug(format_exc())
             return
 
-        if len(s) > self._max_comment_len:
+        if len(comment) > 10:
             self.log.warning(messages.QadbReportCommentLengthWarning())
 
         out.info('please specify rd-qa NIS password')
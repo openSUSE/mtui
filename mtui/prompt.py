1535d8440c6fc06536822860e4ac5beb0e81c8ec
Roman Neuhauser <rneuhauser@suse.cz>
2015-07-29 10:06:49 +0200

    fix NameError, AttributeError in CommandPrompt#do_testsuite_submit
    three bugs in a single change, ffs!
    
    fixes bsc#939695
    broken in 26153694cc166f888aaee2c7e2b8bc5cd6189fc6

--- mtui/prompt.py
+++ mtui/prompt.py
@@ -1402,7 +1402,7 @@ class CommandPrompt(cmd.Cmd):
             self.log.debug(format_exc())
             return
 
-        if len(s) > self._max_comment_len:
+        if len(comment) > 10:
             self.log.warning(messages.QadbReportCommentLengthWarning())
 
         out.info('please specify rd-qa NIS password')
