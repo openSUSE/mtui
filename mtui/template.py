openSUSE/mtui

commit e6af2f72fb3d9d37edc79f535e0301789556498f

reimplement downloading of src.rpm w/o wget
the old implementation was wobbly: it used wget(1) to recurse on links
from the repository http url and download all *.src.rpm files.

the new implementation downloads all src.rpm urls generated by joining
the "repository" metadata with each path from packages-list.txt ending
in ".src.rpm".

the old code was eager to download multiple copies of the same file,
overwriting previous downloads.  this new version downloads a single
copy of each unique src.rpm-ending basename, from whichever url comes
first in the file.

i wanted to get rid of the `rpm2cpio | cpio` / `os.system hack`.  after
wasting another halfday with the horror of librpm and its python bindings
plus sources for both i concluded that this effort was futile as the binding
does not expose rpmReadPackageFile, a crucial (it seems, given the sorry
state of librpm's documentation and the contents of rpm-4.12.0.1/rpm2cpio.c
and rpm-4.12.0.1/rpm2archive.c) piece for this goal.  the hack stays for now.

example inputs:

* 2b1ed4c3c285d4c3707988c48175ea4d/packages-list.txt::

    sle11-i586/docbook_4-4.5-111.16.1.noarch.rpm
    sle11-i586/docbook_4-4.5-111.16.1.src.rpm
    sle11-ia64/docbook_4-4.5-111.16.1.noarch.rpm
    sle11-ia64/docbook_4-4.5-111.16.1.src.rpm
    sle11-ppc64/docbook_4-4.5-111.16.1.noarch.rpm
    sle11-ppc64/docbook_4-4.5-111.16.1.src.rpm
    sle11-s390x/docbook_4-4.5-111.16.1.noarch.rpm
    sle11-s390x/docbook_4-4.5-111.16.1.src.rpm
    sle11-x86_64/docbook_4-4.5-111.16.1.noarch.rpm
    sle11-x86_64/docbook_4-4.5-111.16.1.src.rpm

* SUSE:Maintenance:607:57322/packages-list.txt::

    SUSE_SLE-12_Update/noarch/python-Jinja2-2.7.3-4.1.noarch.rpm
    SUSE_Updates_12-Cloud-Compute_5_x86_64/noarch/python-Jinja2-2.7.3-4.1.noarch.rpm
    SUSE_Updates_SLE-BSK_12_ppc64le/noarch/python-Jinja2-2.7.3-4.1.noarch.rpm
    SUSE_Updates_SLE-BSK_12_s390x/noarch/python-Jinja2-2.7.3-4.1.noarch.rpm

mtui/template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from os.path import join, basename, dirname
+from os.path import join, split, basename, dirname
 from errno import ENOENT
 from errno import EEXIST
 import shutil
@@ -19,13 +19,15 @@ from mtui.refhost import RefhostsFactory
 from mtui.refhost import Attributes
 from mtui.testopia import Testopia
 
+from mtui.five import urlopen
+
 from mtui import utils
 
 from mtui.utils import ensure_dir_exists, chdir
 from mtui.types import MD5Hash
 from mtui.types.obs import RequestReviewID
 from mtui.five import with_metaclass
-from mtui.messages import FailedToDownloadSrcRPMError
+from mtui.messages import QadbReportCommentLengthWarning
 from mtui.messages import FailedToExtractSrcRPM
 from mtui.messages import SrcRPMExtractedMessage
 from mtui.messages import SvnCheckoutInterruptedError
@@ -40,16 +42,6 @@ class _TemplateIOError(IOError):
     else in the process
     """
     pass
-
-def download_source_rpm(uri, recursion_level, system = subprocess.call):
-    cmd = 'wget -q -r -nd -l{1} --no-parent -A "*src.rpm" {0}/'.format(
-        uri,
-        recursion_level
-    )
-    rc = system(cmd, shell = True)
-
-    if rc:
-        raise FailedToDownloadSrcRPMError(rc, cmd)
 
 def testreport_svn_checkout(config, log, uri):
     ensure_dir_exists(
@@ -586,8 +578,31 @@ class TestReport(with_metaclass(ABCMeta, object)):
                     x = s(self, join(d, f), self.log)
                     x.run(targets)
 
+    def download_file(self, from_, into):
+        self.log.info("Downloading %s" % from_)
+        from contextlib import closing
+        with open(into, 'wb') as dst, closing(urlopen(from_)) as src:
+            dst.writelines(src)
+
     def download_source_rpm(self):
-        raise NotImplementedError()
+        try:
+            with open(self.report_wd('packages-list.txt', filepath = True), 'r') as fd:
+                paths = []
+                paths = dict()
+                for line in fd:
+                    tail = line.rstrip()
+                    if not tail.endswith('.src.rpm'): continue
+                    _, fname = split(tail)
+                    if fname in paths: continue
+                    url = '%s/%s' % (self.repository.rstrip('/'), tail)
+                    path = '%s/%s' % (self.local_wd(), basename(tail))
+                    self.download_file(url, into = path)
+                    paths[fname] = path
+                return paths
+        except Exception as e:
+            self.log.error("Failed to download source rpm")
+            self.log.debug(format_exc(e))
+            return []
 
     def extract_source_rpm(self):
         with chdir(self.local_wd()):
@@ -690,6 +705,9 @@ class NullTestReport(TestReport):
         '''python-2.x compat, see __bool__()'''
         return tr.__bool__()
 
+    def download_source_rpm(self):
+        raise NotImplementedError()
+
     def target_wd(self, *paths):
         return join(self.config.target_tempdir, *paths)
 
@@ -727,12 +745,6 @@ class SwampTestReport(TestReport):
 
     def _parser(self):
         return SWAMPMetadataParser()
-
-    def download_source_rpm(self):
-        download_source_rpm(
-            self.repository,
-            2
-        )
 
     def set_repo(self, target, name):
         argv = ('-n',)
@@ -773,12 +785,6 @@ class OBSTestReport(TestReport):
             ('ReviewRequestID'  , self.rrid),
             ('Rating'           , self.rating),
         ] + super(OBSTestReport, self)._show_yourself_data()
-
-    def download_source_rpm(self):
-        download_source_rpm(
-            self.repository,
-            3
-        )
 
     def set_repo(self, target, name):
         argv = ('-z',)

mtui/five.py
@@ -15,3 +15,7 @@ def with_metaclass(meta, *bases):
             return meta(name, bases, d)
     return type.__new__(metaclass, 'temporary_class', (), {})
 
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlopen

mtui/messages.py
@@ -152,9 +152,6 @@ class CompareScriptCrashed(CompareScriptError):
             self.stderr,
         )
 
-class FailedToDownloadSrcRPMError(SystemCommandError):
-    _message = "Failed to download source rpm."
-
 class FailedToExtractSrcRPM(SystemCommandError):
     _message = "Failed to extract source rpm."
 

mtui/refhost.py
@@ -8,10 +8,7 @@ import operator
 import os
 import time
 import errno
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib import urlopen
+from mtui.five import urlopen
 
 from xml.dom import minidom
 from mtui.xdg import save_cache_path

commit 091b39b546ea95fd76c3c55e70c30c439afd1eac

fix error branch of TestReport#download_source_rpm
broken in e6af2f72fb3d9d37edc79f535e0301789556498f

mtui/template.py
@@ -587,7 +587,6 @@ class TestReport(with_metaclass(ABCMeta, object)):
     def download_source_rpm(self):
         try:
             with open(self.report_wd('packages-list.txt', filepath = True), 'r') as fd:
-                paths = []
                 paths = dict()
                 for line in fd:
                     tail = line.rstrip()
@@ -602,7 +601,7 @@ class TestReport(with_metaclass(ABCMeta, object)):
         except Exception as e:
             self.log.error("Failed to download source rpm")
             self.log.debug(format_exc(e))
-            return []
+            return dict()
 
     def extract_source_rpm(self):
         with chdir(self.local_wd()):