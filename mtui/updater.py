openSUSE/mtui

commit c5253f099862d4c5bb16a067e34b64c8f9cc1f07

works with both rep-clean and repose
`export` does not bother matching exact commands anymore,
it just replaces everything below "put here the output (...)"
with the actual commands and their inputs; this means `export`
will work more reliably.

the commands performed by mtui in the course of updating had to
be modified: only those command lines that start with "zypper "
are included; this is to keep the auxiliary commands (`prepare`,
checks) and their outputs from ending up in the log.

mtui/updater.py
@@ -41,26 +41,27 @@ class ZypperUpToSLE11Update(ZypperUpdate):
 
         patch = self.patches['sat']
         self.commands = [
-            'export LANG=',
-            'zypper lr -puU',
-            'zypper refresh',
-            'zypper patches | grep " %s "' % patch,
-            'for p in $(zypper patches | grep " %s " | awk \'BEGIN { FS="|"; } { print $2; }\'); do zypper -n install -l -y -t patch $p=%s; done' % (patch, patch),
+            r"""export LANG=""",
+            r"""zypper lr -puU""",
+            r"""zypper refresh""",
+            r"""zypper patches | grep ' %s '""" % patch,
+            r"""zypper patches | awk -F '|' '/ %s / { print $2; }' | while read p; do zypper -n install -l -y -t patch $p=%s; done""" % (patch, patch),
         ]
 
 
 class ZypperSLE12Update(ZypperUpdate):
     def __init__(self, *a, **kw):
         super(ZypperSLE12Update, self).__init__(*a, **kw)
-        repo = "TESTING-{0}".format(self.testreport.rrid.maintenance_id)
+        repat = ':p=%d' if self.testreport.config.use_repose else 'TESTING-%d'
+        repo = repat % (self.testreport.rrid.maintenance_id)
 
         self.commands = [
-            "export LANG=",
-            "zypper lr -puU",
-            "zypper refresh",
-            "zypper patches | grep {0}".format(repo),
-            "for p in $(zypper patches | grep {0} | awk 'BEGIN {{ FS=\"|\"; }} {{ print $2; }}'); do zypper -n install -l -y -t patch $p; done".format(repo),
-            "zypper patches | grep {0}".format(repo)
+            r"""export LANG=""",
+            r"""zypper lr -puU""",
+            r"""zypper refresh""",
+            r"""zypper patches | grep %s""" % repo,
+            r"""zypper patches | awk -F "|" '/%s\>/ { print $2; }' | while read p; do zypper -n install -l -y -t patch $p; done""" % repo,
+            r"""zypper patches | grep %s""" % repo,
         ]
 
 
@@ -91,11 +92,11 @@ class OldZypperUpdate(Update):
 
         patch = self.patches['zypp']
         self.commands = [
-            'export LANG=',
-            'zypper sl',
-            'zypper refresh',
-            'zypper patches | grep %s-0' % patch,
-            'for p in $(zypper patches | grep %s-0 | awk \'BEGIN { FS="|"; } { print $2; }\'); do zypper -n in -l -y -t patch $p; done' % patch,
+            r"""export LANG=""",
+            r"""zypper sl""",
+            r"""zypper refresh""",
+            r"""zypper patches | grep %s-0""" % patch,
+            r"""zypper patches | awk -F "|" '/%s-0/ { print $2; }') | while read p; do zypper -n in -l -y -t patch $p; done""" % patch,
         ]
 
 class OnlineUpdate(Update):

mtui.spec.in
@@ -30,6 +30,7 @@ Requires:       osc
 Requires:       python-paramiko
 Requires:       python-setuptools
 Requires:       python-xdg >= 0.21
+Requires:       repose
 Requires:       rpm-python
 Recommends:     python-keyring
 Recommends:     python-notify

mtui/config.py
@@ -103,6 +103,9 @@ class Config(object):
             ('session_user', ('mtui', 'user'),
              getpass.getuser),
 
+            ('use_repose', ('mtui', 'use_repose'),
+                False, bool, self.config.getboolean),
+
             # connection.timeout appears to be in units of seconds as
             # indicated by
             # http://www.lag.net/paramiko/docs/paramiko.Channel-class.html#gettimeout

mtui/export.py
@@ -255,20 +255,6 @@ def xml_to_template(logger, template, xmldata, updatehost=None):
     except ValueError:
         out.error('install log section not found in template. skipping.')
     else:
-        command_lines = 1
-
-        # read update commands from the template and search for them in
-        # the xml log. if they were found, add them just below the commands
-        # in the template
-
-        # increment command_lines if a update command was found in the template
-        # ie. if we are in the command section, and the current line is not empty
-        while t[i + command_lines] != '\n':
-            command_lines += 1
-
-        # go to the next newline after the last command in the template
-        current_line = i + command_lines
-
         # if an updatehost was set, search for the update log of that specific host.
         # if none was set, the first found update log is exported to the template.
         if updatehost is not None:
@@ -280,21 +266,19 @@ def xml_to_template(logger, template, xmldata, updatehost=None):
 
         # add hostname to indicate from which host the log was exported
         updatehost = log.parentNode.getAttribute('hostname')
-        t.insert(current_line + 1, "log from %s\n" % updatehost)
 
-        # add the output of each command from bottom to top of the commandlist.
-        # other than from top to bottom, we save some arithmetics with the
-        # current_line/command_lines values this way round.
-        while command_lines:
-            current_line = i + command_lines
-            for child in log.childNodes:
-                try:
-                    if child.getAttribute('name') == t[current_line].strip('\n'):
-                        t.insert(current_line + 1, str(child.childNodes[0].nodeValue).replace('\t', ''))
-                        t[current_line] = '# ' + t[current_line]
-                except Exception:
-                    pass
-            command_lines -= 1
+        t = t[0:i]
+
+        t.append("log from %s\n" % updatehost)
+
+        for child in log.childNodes:
+            if not hasattr(child, 'getAttribute'): continue
+            cmd = child.getAttribute('name')
+            if not cmd.startswith('zypper '): continue
+            t.append('# %s\n%s\n' % (
+                cmd,
+                str(child.childNodes[0].nodeValue),
+            ))
 
     return t
 

mtui/prompt.py
@@ -1882,8 +1882,10 @@ class CommandPrompt(cmd.Cmd):
 
         try:
             template = self.metadata.generate_templatefile(hostname)
-        except Exception:
+        except Exception as e:
             self.log.error('failed to export XML')
+            self.log.error(e)
+            self.log.debug(format_exc(e))
             return
 
         if os.path.exists(filename) and not force:

mtui/target/__init__.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 import os
 import re
 import signal
+import subprocess
 from traceback import format_exc
 
 from mtui.connection import *
@@ -327,6 +328,31 @@ class Target(object):
     def set_repo(self, name, testreport):
         self.logger.debug('{0}: enabling {1} repos'.format(self.hostname, name))
         testreport.set_repo(self, name)
+
+    def run_repose(self, cmd, arg):
+        cmdline = [
+            'repose',
+            cmd,
+            ('root@%s' % (str(self.hostname),)),
+            '--',
+            arg,
+        ]
+        self.logger.info("local/%s: %s" % (self.hostname, ' '.join(cmdline)))
+        cld = subprocess.Popen(
+            cmdline,
+            close_fds = True,
+            stdin  = subprocess.PIPE,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+        )
+        cld.stdin.close()
+        ex = cld.wait()
+        data, logger = \
+            (cld.stdout, self.logger.info) \
+            if ex == 0 else \
+            (cld.stderr, self.logger.error)
+        for l in data:
+            logger("local/%s: %s" % (self.hostname, l.rstrip()))
 
     def run_repclean(self, args):
         exe = self.config.repclean_path

mtui/template.py
@@ -796,7 +796,13 @@ class OBSTestReport(TestReport):
         ] + super(OBSTestReport, self)._show_yourself_data()
 
     def set_repo(self, target, name):
-        argv = ('-z',)
-        if name == 'TESTING':
-            argv = ('-z', '-i', self.rrid.maintenance_id,)
-        target.run_repclean(argv)
+        if self.config.use_repose:
+            if name == 'TESTING':
+                target.run_repose('issue-add', self.report_wd())
+            else:
+                target.run_repose('issue-rm', str(self.rrid))
+        else:
+            argv = ('-z',)
+            if name == 'TESTING':
+                argv = ('-z', '-i', self.rrid.maintenance_id,)
+            target.run_repclean(argv)

tests/test_command_config.py
@@ -47,6 +47,7 @@ def test_config_show():
             , ("template_dir"               , "'foo-template'")
             , ("local_tempdir"              , "'/tmp'")
             , ("session_user"               , "'foo-user'")
+            , ("use_repose"                 , "False")
             , ("connection_timeout"         , "300")
             , ("svn_path"                   , "'svn+ssh://svn@qam.suse.de/testreports'")
             , ("bugzilla_url"               , "'https://bugzilla.novell.com'")

commit 3b68debfb6b3f3ef2fcb11677a9395aaa1e2614a

fix a syntax error in a bash fragment in OldZypperUpdate
i broke it in c5253f099862d4c5bb16a067e34b64c8f9cc1f07

mtui/updater.py
@@ -96,7 +96,7 @@ class OldZypperUpdate(Update):
             r"""zypper sl""",
             r"""zypper refresh""",
             r"""zypper patches | grep %s-0""" % patch,
-            r"""zypper patches | awk -F "|" '/%s-0/ { print $2; }') | while read p; do zypper -n in -l -y -t patch $p; done""" % patch,
+            r"""zypper patches | awk -F "|" '/%s-0/ { print $2; }' | while read p; do zypper -n in -l -y -t patch $p; done""" % patch,
         ]
 
 class OnlineUpdate(Update):