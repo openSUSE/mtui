e99373bd2e289937171198a0e3911424808b7da7
Ondřej Súkup <osukup@suse.cz>
2016-11-01 15:27:26 +0100

    Fix check for local_tmpdir value..

--- tests/test_command_config.py
+++ tests/test_command_config.py
@@ -10,6 +10,7 @@ try:
 except ImportError:
     from itertools import izip_longest as zip_longest
 
+
 def test_config():
     """
     Test
@@ -20,6 +21,7 @@ def test_config():
     cp.onecmd("config")
     eq_(cp.stdout.getvalue(), "usage: config [-h] {show} ...\n")
 
+
 def test_config_show():
     """
     Test
@@ -29,43 +31,20 @@ def test_config_show():
 
     c = ConfigFake(dict(
         # redefine options which depend on environment
-        datadir = 'foo-data'
-      , template_dir = 'foo-template'
-      , refhosts_path = 'foo-refhosts'
-      , session_user = 'foo-user'
-    ))
+        datadir='foo-data', template_dir='foo-template', local_tempdir='/tmp/mtui-unittests', refhosts_path='foo-refhosts', session_user='foo-user'
+        ))
 
-    cp = make_cp(config = c)
+    cp = make_cp(config=c)
     cp.onecmd("config show")
 
-    for actual,expected in zip_longest(
-          [ (opt, val)
+    for actual, expected in zip_longest(
+        [(opt, val)
             for x in cp.stdout.getvalue().splitlines()
-            for opt, _, val in [x.partition(" = ")]]
-        , [("{0:<25}".format(opt), val) for (opt, val) in
-            [ ("datadir"                    , "'foo-data'")
-            , ("template_dir"               , "'foo-template'")
-            , ("local_tempdir"              , "'/tmp'")
-            , ("session_user"               , "'foo-user'")
-            , ("connection_timeout"         , "300")
-            , ("svn_path"                   , "'svn+ssh://svn@qam.suse.de/testreports'")
-            , ("bugzilla_url"               , "'https://bugzilla.novell.com'")
-            , ("reports_url"                , "'http://qam.suse.de/testreports'")
-            , ("target_tempdir"             , "'/tmp'")
-            , ("target_testsuitedir"        , "'/usr/share/qa/tools'")
-            , ("testopia_interface"         , "'https://apibugzilla.novell.com/xmlrpc.cgi'")
-            , ("testopia_user"              , "''")
-            , ("testopia_pass"              , "''")
-            , ("chdir_to_template_dir"      , "False")
-            , ("refhosts_resolvers"         , "'https'")
-            , ("refhosts_https_uri"         , "'https://qam.suse.de/metadata/refhosts.xml'")
-            , ("refhosts_https_expiration"  , "43200")
-            , ("refhosts_path"              , "'foo-refhosts'")
-            , ("use_keyring"                , 'False')
-            , ('report_bug_url'             , "'https://bugzilla.suse.com/enter_bug.cgi?classification=40&product=Testenvironment&submit=Use+This+Product&component=MTUI'")
-            , ("location"                   , "'default'")
-        ]]):
-            eq_(actual,expected)
+            for opt, _, val in [x.partition(" = ")]], [("{0:<25}".format(opt), val) for (opt, val) in
+                                                       [("datadir", "'foo-data'"), ("template_dir", "'foo-template'"), ("local_tempdir", "'/tmp/mtui-unittests'"), ("session_user", "'foo-user'"), ("connection_timeout", "300"), ("svn_path", "'svn+ssh://svn@qam.suse.de/testreports'"), ("bugzilla_url", "'https://bugzilla.novell.com'"), ("reports_url", "'http://qam.suse.de/testreports'"), ("target_tempdir", "'/tmp'"), ("target_testsuitedir", "'/usr/share/qa/tools'"), ("testopia_interface", "'https://apibugzilla.novell.com/xmlrpc.cgi'"), ("testopia_user", "''"), ("testopia_pass", "''"), ("chdir_to_template_dir", "False"), ("refhosts_resolvers", "'https'"), ("refhosts_https_uri", "'https://qam.suse.de/metadata/refhosts.xml'"), ("refhosts_https_expiration", "43200"), ("refhosts_path", "'foo-refhosts'"), ("use_keyring", 'False'), ('report_bug_url', "'https://bugzilla.suse.com/enter_bug.cgi?classification=40&product=Testenvironment&submit=Use+This+Product&component=MTUI'"), ("location", "'default'")
+                                                        ]]):
+        eq_(actual, expected)
+
 
 def test_config_show_one():
     """
@@ -75,8 +54,8 @@ def test_config_show_one():
     """
 
     c = ConfigFake(dict(
-        datadir = 'foo-data'
+        datadir='foo-data'
     ))
-    cp = make_cp(config = c)
+    cp = make_cp(config=c)
     cp.onecmd("config show datadir")
     eq_(cp.stdout.getvalue(), "datadir = 'foo-data'\n")
