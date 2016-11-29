28a1ab62fdfd7922f3dc2c29470f6d6b1ff3d58b
Ondřej Súkup <osukup@suse.cz>
2016-11-29 21:20:29 +0100

    Added sap-aio to get_release funcion

--- mtui/utils.py
+++ mtui/utils.py
@@ -278,10 +278,9 @@ def get_release(systems):
     for rexp, release in {
         'rhel': 'YUM',
         'sle[sd]12': '12',
+        'sap-aio12': '12',
         '(manager|mgr|sles4vmware|cloud|studio|slms|sle.11)': '11',
-        'sle.10': '10',
-        'sle.9': 9,
-        'sl11': '114',
+        'sl11': '114'
     }.items():
         if re.search(rexp, systems):
             return release
