12dc5af0b85d9e880193b2e750c66f6f4e1e1059
Ondřej Súkup <osukup@suse.cz>
2016-11-29 12:21:47 +0100

    Release v6.2.6

--- mtui/__init__.py
+++ mtui/__init__.py
@@ -2,7 +2,7 @@ from distutils.version import StrictVersion
 
 __all__ = ['main', 'log', 'export']
 
-__version__ = '6.2.5'
+__version__ = '6.2.6'
 # PEP396
 
 strict_version = StrictVersion(__version__)
