fbc023ea3f45244bbc29af69751fcc7383415481
Ondřej Súkup <osukup@suse.cz>
2017-01-27 22:09:05 +0100

    Splited definition of CLI arguments from main.

---
+++ mtui/args.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from argparse import FileType
+
+from .argparse import ArgumentParser
+from mtui.template import OBSUpdateID
+
+
+from mtui import __version__
+
+
+def get_parser(sys):
+    """
+    :covered-by: tests.test_main.test_argparser_*
+    """
+
+    parser = ArgumentParser(sys_=sys)
+    parser.add_argument(
+        '-l', '--location',
+        type=str,
+        help='override config mtui.location'
+    )
+    parser.add_argument(
+        '-t', '--template_dir',
+        type=str,
+        help='override config mtui.template_dir'
+    )
+    parser.add_argument(
+        '-r', '--review-id',
+        type=OBSUpdateID,
+        help='OBS request review id\nexample: SUSE:Maintenance:1:1'
+    )
+    parser.add_argument(
+        '-s', '--sut',
+        type=str,
+        action='append',
+        help='cumulatively override default hosts from template \n'
+        "format: hostname,system"
+    )
+    parser.add_argument(
+        '-p', '--prerun',
+        type=FileType('r'),
+        help='script with a set of MTUI commands to run at start'
+    )
+    parser.add_argument(
+        '-w', '--connection_timeout',
+        type=int,
+        help='override config mtui.connection_timeout'
+    )
+    parser.add_argument(
+        '-n', '--noninteractive',
+        action='store_true',
+        default=False,
+        help='noninteractive update shell'
+    )
+    parser.add_argument(
+        '-d', '--debug',
+        action='store_true',
+        default=False,
+        help='enable debugging output'
+    )
+    parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version='{}'.format(__version__),
+        help='print version and exit'
+    )
+
+    return parser
