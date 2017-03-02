5b99d61e52397021575e37fd958d8c81f547a84c
Ondřej Súkup <osukup@suse.cz>
2017-03-02 20:39:19 +0100

    Release bugfix version 7.1.4

--- mtui/__init__.py
+++ mtui/__init__.py
@@ -2,7 +2,7 @@ from distutils.version import StrictVersion
 
 __all__ = ['main', 'log', 'export']
 
-__version__ = '7.1.3'
+__version__ = '7.1.4'
 # PEP396
 
 strict_version = StrictVersion(__version__)
