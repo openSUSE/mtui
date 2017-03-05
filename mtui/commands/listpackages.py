276c83a48c549ca2bfca62bce7df91c9afa5b72a
Ondřej Súkup <osukup@suse.cz>
2017-03-05 23:13:53 +0100

    Fixed list_packages command
    In python3 isn't cmp command, used (a > b) - (a < b)
    
    Version is explicitly converted to string

--- mtui/commands/listpackages.py
+++ mtui/commands/listpackages.py
@@ -23,7 +23,7 @@ class ListPackages(Command):
         if not current:
             return self.state_map[None]
 
-        return self.state_map[cmp(current, wanted)]
+        return self.state_map[(current > wanted) - (current < wanted)]
 
     @classmethod
     def _add_arguments(cls, parser):
@@ -85,7 +85,7 @@ class ListPackages(Command):
             self.println()
 
     def printPVLN(self, package, version, state):
-        self.println('{0:30}: {1:15} {2}'.format(package, version, state))
+        self.println('{0:30}: {1!s:20} {2}'.format(package, version, state))
 
     @staticmethod
     def complete(state, text, line, begidx, endidx):
