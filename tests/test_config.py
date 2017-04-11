5c85000c3794fbc941381a930438abc42c087995
dasantiago <dasantiago@suse.cz>
2017-04-11 10:18:41 +0200

    Some basic tests for mtui.

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,8 +5,10 @@ from nose.tools import ok_
 from nose.tools import raises
 
 from mtui.config import InvalidOptionNameError
-
+import configparser
+import os
 from .utils import ConfigFake
+from mtui.commands.config import Config
 
 class ArgsFake(object):
     def __init__(self):
@@ -27,3 +29,33 @@ def test_merge_args():
 def test_set_option_invalid():
     c = ConfigFake()
     c.set_option('foobar', 'kek')
+
+def test_config_parse():
+    cfg = ConfigFake()
+
+    cfg.config = configparser.SafeConfigParser()
+    cfg._define_config_options()
+    cfg.configfiles=[_find_config_file('basic_config.ini')]
+    cfg.config.read(cfg.configfiles)
+    cfg._parse_config()
+
+    eq_(cfg.datadir, '/test/location')
+    eq_(cfg.location, 'foolocation')
+
+    eq_(cfg.testopia_interface, 'https://test.api')
+    eq_(cfg.testopia_user, 'test_user')
+    eq_(cfg.testopia_pass, 'test_password')
+
+    eq_(cfg.refhosts_resolvers, 'https')
+    eq_(cfg.refhosts_https_uri, 'https://test_remote/refhosts.yml')
+    eq_(cfg.refhosts_path, '/path/to/refhosts/file')
+
+    eq_(cfg.bugzilla_url, 'https://bugzilla.test')
+
+def _find_config_file(name):
+    return os.path.join(
+        os.path.dirname(__file__)
+        , "fixtures"
+        , "config"
+        , name
+    )
