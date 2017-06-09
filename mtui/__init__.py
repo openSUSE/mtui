1fcf3f55e17c86c803daf2aef03e36822ed2eb20
Ondřej Súkup <osukup@suse.cz>
2017-06-09 17:21:52 +0200

    Release version 8.0.5

--- mtui/__init__.py
+++ mtui/__init__.py
@@ -2,7 +2,7 @@ from distutils.version import StrictVersion
 
 __all__ = ['main', 'log', 'export']
 
-__version__ = '8.0.4'
+__version__ = '8.0.5'
 # PEP396
 
 strict_version = StrictVersion(__version__)
