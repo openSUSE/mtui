c959aa1982cd8dc402f53e7cb257836dfadec0a3
Ondřej Súkup <osukup@suse.cz>
2017-08-24 09:30:46 +0200

    Mark version 8.1.2

--- mtui/__init__.py
+++ mtui/__init__.py
@@ -2,7 +2,7 @@ from distutils.version import StrictVersion
 
 __all__ = ['main', 'log', 'export']
 
-__version__ = '8.1.1'
+__version__ = '8.1.2'
 # PEP396
 
 strict_version = StrictVersion(__version__)
