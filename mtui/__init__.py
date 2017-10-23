2a3d80effb9e80ca8aa74ab76dff5194888c5417
Ondřej Súkup <osukup@suse.cz>
2017-10-23 14:49:59 +0200

    Release bugfix version 8.1.6

--- mtui/__init__.py
+++ mtui/__init__.py
@@ -2,7 +2,7 @@ from distutils.version import StrictVersion
 
 __all__ = ['main', 'log', 'export']
 
-__version__ = '8.1.5'
+__version__ = '8.1.6'
 # PEP396
 
 strict_version = StrictVersion(__version__)
