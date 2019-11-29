d04ac245ad591ce8242b0f9ed877b877047b24ef
Ondřej Súkup <osukup@suse.com>
2019-11-29 12:33:38 +0100

    Remove 'save' command

--- mtui/commands/quit.py
+++ mtui/commands/quit.py
@@ -3,14 +3,12 @@ import readline
 
 from mtui.commands import Command
 from mtui.utils import complete_choices
-from mtui.utils import prompt_user
 
 
 class Quit(Command):
     """
     Disconnects from all hosts and exits the programm.
     If a bootarg  argument is set, the hosts are either rebooted or powered off.
-    The tester is asked to save the XML log when exiting MTUI.
     """
 
     command = "quit"
@@ -30,13 +28,6 @@ class Quit(Command):
         self.targets.pop(target)
 
     def __call__(self):
-
-        if not prompt_user(
-            "save log? (Y,n) ",
-            ["n", "no", "N", "No", "NO", "nein", "ne"],
-            self.prompt.interactive,
-        ):
-            self.prompt._do_save_impl()
 
         args_ = [self.args.bootarg] if self.args.bootarg else []
 
