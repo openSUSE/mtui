655cc6dab517d100d004a4e7a01e2c3a3421f341
Ondřej Súkup <osukup@suse.com>
2019-11-29 14:47:17 +0100

    Add new 'reload_openqa' command

---
+++ mtui/commands/reloadoqa.py
@@ -0,0 +1,57 @@
+from logging import getLogger
+
+from ..connector.openqa import AutoOpenQA, KernelOpenQA
+from ..utils import complete_choices, requires_update
+from . import Command
+
+logger = getLogger("mtui.commands.reloadopenqa")
+
+
+class ReloadOpenQA(Command):
+    """
+    Reload's informations from openQA instances
+    """
+
+    command = "reload_openqa"
+
+    @requires_update
+    def __call__(self):
+        if self.config.kernel:
+            if self.metadata.openqa["kernel"] == []:
+                logger.info("Getting data from kernel openQA")
+                self.metadata.openqa["kernel"].append(
+                    KernelOpenQA(
+                        self.config,
+                        self.config.openqa_instance,
+                        self.metadata.smelt,
+                        self.metadata.id,
+                    ).run()
+                )
+                self.metadata.openqa["kernel"].append(
+                    KernelOpenQA(
+                        self.config,
+                        self.config.openqa_instance_baremetal,
+                        self.metadata.smelt,
+                        self.metadata.id,
+                    ).run()
+                )
+            else:
+                logger.info("Refreshing data from kernel openQA")
+                for oqa in self.metadata.openqa["kernel"]:
+                    oqa.run()
+
+        if self.metadata.openqa["auto"] is None:
+            logger.info("Getting data from openQA")
+            self.metadata.openqa["auto"] = AutoOpenQA(
+                self.config,
+                self.config.openqa_instance,
+                self.metadata.smelt,
+                self.metadata.id,
+            ).run()
+        else:
+            logger.info("Refreshing data from openQA")
+            self.metadata.openqa["auto"].run()
+
+    @staticmethod
+    def complete(_, text, line, begix, endix):
+        return complete_choices([], line, text)
