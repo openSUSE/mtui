92c6c4f6e9d89364eb356bd6fbc50f0dbec15ab1
Ondřej Súkup <osukup@suse.com>
2020-11-09 10:37:15 +0000

    Revert "Remove dead attribute from CommandPrompt"
    This reverts commit 95419961fd6a0b0eb84fb9a33f801e262db4c296

--- mtui/main.py
+++ mtui/main.py
@@ -12,10 +12,9 @@ from mtui.systemcheck import detect_system
 from .argparse import ArgsParseFailure
 from .colorlog import create_logger
 
-logger = create_logger("mtui")
-
 
 def main():
+    logger = create_logger("mtui")
 
     p = get_parser(sys)
     try:
@@ -30,10 +29,10 @@ def main():
 
     cfg = Config(args.config)
 
-    sys.exit(run_mtui(sys, cfg, CommandPrompt, CommandPromptDisplay, args))
+    sys.exit(run_mtui(sys, cfg, logger, CommandPrompt, CommandPromptDisplay, args))
 
 
-def run_mtui(sys, config, Prompt, Display, args):
+def run_mtui(sys, config, logger, Prompt, Display, args):
 
     if args.debug:
         logger.setLevel(level=logging.DEBUG)
@@ -44,7 +43,7 @@ def run_mtui(sys, config, Prompt, Display, args):
 
     config.distro, config.distro_ver, config.distro_kernel = detect_system()
 
-    prompt = Prompt(config, sys, Display)
+    prompt = Prompt(config, logger, sys, Display)
     if args.update:
         if args.update.kind == "kernel":
             config.kernel = True
