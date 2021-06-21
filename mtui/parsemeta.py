6a1141a77fa078f307e67e697bff7287dc7bbc28
Ondřej Súkup <osukup@suse.cz>
2021-06-21 15:10:24 +0200

    Fixes and cleanup in MetadataParser

--- mtui/parsemeta.py
+++ mtui/parsemeta.py
@@ -4,24 +4,21 @@ from .types.obs import RequestReviewID
 
 
 class MetadataParser:
-    def parse_line(self, results, line):
-        """
-        :returns: bool True if line was parsed, otherwise False
-        """
+    def parse_line(self, results, line: str) -> None:
         match = re.search("Products: (.+)", line)
         if match:
             results.products = match.group(1).replace("), ", ")|").split("|")
-            return True
+            return
 
         match = re.search("Category: (.+)", line)
         if match:
             results.category = match.group(1)
-            return True
+            return
 
         match = re.search("Packager: (.+)", line)
         if match:
             results.packager = match.group(1)
-            return True
+            return
 
         match = re.search("Packages: (.+)", line)
         if match:
@@ -48,60 +45,59 @@ class MetadataParser:
         match = re.search("Test Plan Reviewer(?:s)?: (.+)", line)
         if match:
             results.reviewer = match.group(1)
-            return True
+            return
 
-        match = re.search('Jira ([A-Z]+-\d+) \("(.*)"\):', line)  # deprecated
+        match = re.search(r'Jira ([A-Z]+-\d+) \("(.*)"\):', line)
         if match:
             results.jira[match.group(1)] = match.group(2)
-            return True
+            return
 
-        match = re.search('Bug (\d+) \("(.*)"\):', line)  # deprecated
+        match = re.search(r'Bug (\d+) \("(.*)"\):', line)
         if match:
             results.bugs[match.group(1)] = match.group(2)
-            return True
+            return
 
         match = re.search("Testplatform: (.*)", line)
         if match:
             results.testplatforms.append(match.group(1))
-            return True
+            return
 
-        match = re.search(".* \(reference host: (\S+).*\)", line)
+        match = re.search(r".* \(reference host: (\S+).*\)", line)
         if match:
             if "?" not in match.group(1):
                 results.hostnames.add(match.group(1))
-                return True
+                return
 
         match = re.search("Bugs: (.*)", line)
         if match:
             for bug in match.group(1).split(","):
                 results.bugs[bug.strip(" ")] = "Description not available"
-            return True
+            return
 
         match = re.search("Jira: (.*)", line)
         if match:
             for issue in match.group(1).split(","):
                 results.jira[issue.strip(" ")] = "Description not available"
-            return True
+            return
 
         m = re.match("Repository: (.+)", line)
         if m:
             results.repository = m.group(1)
-            return True
-
-        return False
+            return
+        return
 
 
 class OBSMetadataParser(MetadataParser):
-    def parse_line(self, results, line):
-        if super(OBSMetadataParser, self).parse_line(results, line):
-            return True
-
+    def parse_line(self, results, line: str) -> None:
         m = re.match("Rating: (.+)", line)
         if m:
             results.rating = m.group(1)
-            return True
+            return
 
         m = re.match("ReviewRequestID: (.+)", line)
         if m:
             results.rrid = RequestReviewID(m.group(1))
-            return True
+            return
+
+        # continue with parernt parse
+        return super().parse_line(results, line)
