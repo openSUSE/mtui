c3a6b9a9560b30f4d9e8d5e2e092769d01652e79
Ondřej Súkup <osukup@suse.com>
2022-07-13 12:15:56 +0200

    Add JSONParse for new metadata format
    Has same interface as MetadataParser class.

---
+++ mtui/parsemetajson.py
@@ -0,0 +1,25 @@
+from .types.obs import RequestReviewID
+
+
+class JSONParser:
+    @staticmethod
+    def parse(results, data):
+        for i in data.get("jira"):
+            results.jira[i] = "Description not available"
+
+        for i in data.get("bugs"):
+            results.bugs[i] = "Description not available"
+
+        results.rrid = RequestReviewID(data.get("rrid"))
+        results.packager = data.get("packager")
+        results.rating = data.get("rating")
+        results.repository = data.get("repository")
+        results.category = data.get("category")
+        results.testplatforms = data.get("testplatform")
+        results.products = data.get("products")
+
+        packages = {}
+        for prod, pkgvers in data.get("packages").items():
+            pkgs = {pkg: ver for pkg, _, ver in (p.split() for p in pkgvers)}
+            packages[prod] = pkgs
+        results.packages = packages
