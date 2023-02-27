55428b250c144a03583e80db01a270c2c671c41e
Ondřej Súkup <osukup@suse.com>
2023-02-27 16:50:51 +0100

    Add support for EXTREME-CORE

--- mtui/template/products/sle11.py
+++ mtui/template/products/sle11.py
@@ -1,6 +1,5 @@
 def normalize_sle11(x):
     """Normalize SLE 11 Products"""
-
     if x[0][0] == "SLE-SDK":
         x[0][0] = "sle-sdk"
         return x
@@ -8,12 +7,15 @@ def normalize_sle11(x):
         x[0][0] = "SUSE_SLES_SAP"
         return x
     if x[0][0] == "SLE-SERVER" and (
-        x[0][1].split("-")[-1] not in ("TERADATA", "SECURITY", "PUBCLOUD")
+        x[0][1].split("-")[-1] not in ("TERADATA", "SECURITY", "PUBCLOUD", "CORE")
     ):
         x[0][0] = "SUSE_SLES"
         x[0][1] = x[0][1].replace("-LTSS", "")
         x[0][1] = x[0][1].replace("-CLIENT-TOOLS", "")
         return x
+    if x[0][1].endswith("CORE"):
+        x[0][0] = "SUSE_SLES_LTSS-EXTREME-CORE"
+        x[0][1] = x[0][1].replace("-LTSS-EXTREME-CORE", "")
     if x[0][1].endswith("TERADATA"):
         x[0][0] = "teradata"
         x[0][1] = x[0][1].replace("-TERADATA", "")
