af20fec0a46849a188debd87a56b703ba05aa6c9
Ondřej Súkup <osukup@suse.com>
2023-08-27 17:33:09 +0200

    Add test for _write deduplication in Exporter

---
+++ tests/test_misc.py
@@ -0,0 +1,38 @@
+import logging
+
+from mtui.export.base import BaseExport
+
+
+class FakeExport(BaseExport):
+    def get_logs(self, *args, **kwds):
+        return None
+
+    def run(self, *args, **kwds):
+        return None
+
+
+def test_duplicate_found(caplog, log_install) -> None:
+    caplog.set_level(logging.INFO, logger="mtui.export.base")
+
+    fn = log_install
+    data = fn.read_text().split("\n")
+
+    export = FakeExport("", "", [], False, "123", True)
+
+    export._writer(fn, data)
+
+    assert caplog.records[0].msg == f"Log {fn} exists and is same as export"
+
+
+def test_diffeerent_files(caplog, log_install) -> None:
+    caplog.set_level(logging.INFO, logger="mtui.export.base")
+
+    fn = log_install
+    data = ["Non duplicate text of install log", "fake log"]
+
+    export = FakeExport("", "", [], False, "123", True)
+
+    export._writer(fn, data)
+
+    assert caplog.records[0].msg == f"file {fn} exists."
+    assert caplog.records[1].msg.startswith("exporting log to")
