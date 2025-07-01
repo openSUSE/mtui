f99611637ef11a7b5a85c49314df94b46f1bd4e9
Ondřej Súkup <osukup@suse.com>
2025-07-01 20:47:44 +0200

    Add SL-Micro support for downgrade

--- mtui/actions/downgrade.py
+++ mtui/actions/downgrade.py
@@ -23,6 +23,29 @@ done \
     }
 
 
+def slmicro() -> dict[str, Template]:
+    list_command_template = r"""
+for p in $packages; do \
+zypper -n se -s --match-exact -t package $$p; \
+done \
+| grep -v "(System" \
+| grep ^[iv] \
+| sed "s, ,,g" \
+| awk -F "|" '{{ print $2,"=",$4 }}'
+"""
+
+    cmd_template = "rpm -q $package &>/dev/null && transactional-update -c pkg in -C --force-resolution -y $package=$version"
+    reboot = "systemctl reboot"
+    init_snapshot = "transactional-update run true"
+
+    return {
+        "list_command": Template(list_command_template),
+        "command": Template(cmd_template),
+        "reboot": Template(reboot),
+        "init_snapshot": Template(init_snapshot),
+    }
+
+
 yum = {"command": Template("yum -y downgrade $package")}
 
 
@@ -32,6 +55,7 @@ downgrader = DictWithInjections(
         ("12", False): zypper(),
         ("15", False): zypper(),
         ("YUM", False): yum,
+        ("slmicro", True): slmicro(),
     },
     key_error=MissingDowngraderError,
 )
