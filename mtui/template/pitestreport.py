d7c50e7c7a80489b4cfeecb42d39f4947a8fba1a
Ondřej Súkup <osukup@suse.com>
2025-08-31 13:44:27 +0200

    Add PI testreport template support

---
+++ mtui/template/pitestreport.py
@@ -0,0 +1,67 @@
+from typing import final
+
+from ..parsemeta import MetadataParser, ReducedMetadataParser
+from ..parsemetajson import JSONParser
+from ..repoparse import reporepoparse
+from ..target import Target
+from ..target.hostgroup import HostsGroup
+from ..template.testreport import TestReport
+from ..types import Product, RequestReviewID
+
+
+@final
+class PITestReport(TestReport):
+    def __init__(self, *a, **kw) -> None:
+        super().__init__(*a, **kw)
+
+        self.rrid: RequestReviewID
+        self.rating = ""
+        self.realid = ""
+        self.repositories: frozenset = frozenset()
+        self._attrs += ["rrid", "rating", "realid"]
+
+    @property
+    def _type(self) -> str:
+        return "PI"
+
+    @property
+    def id(self) -> str:
+        return str(self.rrid)
+
+    def _parser(self):
+        parsers = {
+            "full": MetadataParser,
+            "hosts": ReducedMetadataParser,
+            "json": JSONParser,
+        }
+        return parsers
+
+    def _update_repos_parser(self) -> dict[Product, str]:
+        return reporepoparse(self.repositories, self.products)
+
+    def _show_yourself_data(self) -> list[tuple[str, str]]:
+        return (
+            [
+                ("ReviewRequestID", str(self.rrid)),
+                ("Rating", self.rating),
+                ("Real ID", self.realid),
+            ]
+            + [("Repo", x) for x in self.repositories]
+            + super()._show_yourself_data()
+        )
+
+    def set_repo(self, target: Target, operation: str) -> None:
+        if operation == "add":
+            target.run_zypper("-n ar -cfGkn", self.update_repos, self.rrid)
+        elif operation == "remove":
+            target.run_zypper("-n rr", self.update_repos, self.rrid)
+        else:
+            raise ValueError("Not supported repose operation {}".format(operation))
+
+    def list_update_commands(self, targets: HostsGroup, display) -> None:
+        packages = self.get_package_list()
+        repa = f":p={self.rrid.maintenance_id}:{self.rrid.review_id}"
+        for hn, t in targets.items():
+            display(
+                f"{hn} - commands: \n{t.get_updater()['command'].safe_substitute(repa=repa, packages=packages)}"
+            )
