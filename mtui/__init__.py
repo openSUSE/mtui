783ac0ca8ebe9dd9e0e8f10efdfae407720d6fb2
Ondřej Súkup <osukup@suse.com>
2025-11-11 11:06:44 +0100

    Mark new development version 16.1.0

--- mtui/__init__.py
+++ mtui/__init__.py
@@ -6,7 +6,7 @@ multiple hosts in parallel, with a focus on maintenance update testing.
 
 from looseversion import LooseVersion
 
-__version__ = "16.0.0"
+__version__ = "16.1.0"
 
 # PEP396
 loose_version = LooseVersion(__version__)
